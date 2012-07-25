81289d7d8e847f9585b0534f7bb71a00b5d4d845
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2012-07-25 12:11:47 -0400

    Syncing index to local working directory

--- war/genfilesforspeedtracer/com/fave100/client/pagefragments/TopBarView_BinderImpl_GenCss_style.java
+++
@@ -1,8 +0,0 @@
-package com.fave100.client.pagefragments;
-
-import com.google.gwt.resources.client.CssResource;
-
-public interface TopBarView_BinderImpl_GenCss_style extends CssResource {
-  String faveLogo();
-  String topNav();
-}
--- war/genfilesforspeedtracer/com/fave100/client/pages/home/HomeView_BinderImpl_GenCss_style.java
+++
@@ -1,6 +0,0 @@
-package com.fave100.client.pages.home;
-
-import com.google.gwt.resources.client.CssResource;
-
-public interface HomeView_BinderImpl_GenCss_style extends CssResource {
-}
