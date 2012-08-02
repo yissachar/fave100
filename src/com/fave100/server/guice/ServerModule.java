cacd4d60ac63354fd33478ac2ab2283d2e81021e
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2012-08-02 17:00:14 -0400

    Moved Objectify class registration to ServerModule

--- src/com/fave100/server/guice/ServerModule.java
+++ src/com/fave100/server/guice/ServerModule.java
@@ -1,9 +1,21 @@
 package com.fave100.server.guice;
 
+import com.fave100.server.domain.AppUser;
+import com.fave100.server.domain.FaveItem;
+import com.fave100.server.domain.GoogleID;
+import com.fave100.server.domain.Song;
+import com.googlecode.objectify.ObjectifyService;
 import com.gwtplatform.dispatch.server.guice.HandlerModule;
 
 public class ServerModule extends HandlerModule {
 
+	static{
+		ObjectifyService.register(FaveItem.class);
+		ObjectifyService.register(AppUser.class);
+		ObjectifyService.register(Song.class);
+		ObjectifyService.register(GoogleID.class);
+	}
+	
 	@Override
 	protected void configureHandlers() {
 	}
