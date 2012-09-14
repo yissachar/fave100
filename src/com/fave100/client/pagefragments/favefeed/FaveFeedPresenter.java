42b479d510b6c8fa84291567987d2c1921e1233f
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2012-09-14 16:26:47 -0400

    Style changes

--- src/com/fave100/client/pagefragments/favefeed/FaveFeedPresenter.java
+++ src/com/fave100/client/pagefragments/favefeed/FaveFeedPresenter.java
@@ -46,8 +46,9 @@ public class FaveFeedPresenter extends
 		faveFeedReq.fire(new Receiver<List<String>>() {
 			@Override
 			public void onSuccess(final List<String> faveFeed) {
+				// TODO: Should be a widget
 				final SafeHtmlBuilder builder = new SafeHtmlBuilder();
-				builder.appendHtmlConstant("<h4>");
+				builder.appendHtmlConstant("<h4 class = 'itemHeader'>");
 				builder.appendEscaped("FaveFeed");
 				builder.appendHtmlConstant("</h4>");
 				if(faveFeed.size() > 0) {
