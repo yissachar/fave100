b99a3acee7304e7db00b3f09354be27335a13fad
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2012-10-29 14:26:46 -0400

    Everything encoded with UTF-8 - fixes #1
    Former-commit-id: cf217a8cc9aa9a4911791987643701569cfdfafd

---
+++ src/com/fave100/server/filters/EncodingFilter.java
@@ -0,0 +1,40 @@
+package com.fave100.server.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class EncodingFilter implements Filter {
+
+	private String encoding = "utf-8";
+
+	@Override
+	public void doFilter(final ServletRequest request,
+
+	final ServletResponse response, final FilterChain filterChain) throws IOException, ServletException {
+		request.setCharacterEncoding(encoding);
+		response.setCharacterEncoding(encoding);
+		filterChain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(final FilterConfig filterConfig)
+
+	throws ServletException {
+		final String encodingParam = filterConfig.getInitParameter("encoding");
+		if (encodingParam != null) {
+			encoding = encodingParam;
+		}
+	}
+
+	@Override
+	public void destroy() {
+		// nothing todo
+	}
+
+}
