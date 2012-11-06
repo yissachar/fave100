2863b2cbae2962186c91e08da61a0e86bcaf2da6
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2012-11-06 12:49:32 -0500

    Switched to parsing JSON instead of XML
    Former-commit-id: 366683d3eb11efed73879f802ada8af33a982892

--- src/com/fave100/client/pages/search/MusicbrainzResultList.java
+++ src/com/fave100/client/pages/search/MusicbrainzResultList.java
@@ -1,83 +1,26 @@
 package com.fave100.client.pages.search;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
-import com.google.gwt.xml.client.DOMException;
-import com.google.gwt.xml.client.Document;
-import com.google.gwt.xml.client.Element;
-import com.google.gwt.xml.client.Node;
-import com.google.gwt.xml.client.NodeList;
-import com.google.gwt.xml.client.XMLParser;
+import com.google.gwt.core.client.JsArray;
 
 @SuppressWarnings("serial")
 public class MusicbrainzResultList extends ArrayList<MusicbrainzResult>{
 	
-	public MusicbrainzResultList(final String data) {
-		try {
-    	    // parse the XML document into a DOM
-    	    final Document messageDom = XMLParser.parse(data);	
-    	    
-    	    // Get all results
-    	    final NodeList recordingNodes = messageDom.getElementsByTagName("recording");
-    	    final int length = recordingNodes.getLength();
-    	    
-    	    // Hash map to ensure we only show one results per songTitle:Artist
-    	    final HashMap<String, String> uniqueSongMap = new HashMap<String, String>();
-    	    
-    	    for(int i = 0; i < length; i++) {
-    	    	
-    	    	String mbid = "";
-    	    	String track = "";
-    	    	String artist = "";
-    	    	String releaseDate = "";
-    	    	
-    	    	final Element recordingElement = (Element) messageDom.getElementsByTagName("recording").item(i);
-    	    	
-    	    	// Musicbrainz ID
-    	    	mbid = recordingElement.getAttribute("id");
-    	    	
-    	    	// Track name
-    	    	final Element titleElement = (Element) recordingElement.getElementsByTagName("title").item(0);
-    	    	track = titleElement.getChildNodes().item(0).getNodeValue();
-    	    	
-    	    	// Artist name
-    	    	final Element artistElement = (Element) recordingElement.getElementsByTagName("artist").item(0);
-    	    	if(artistElement != null) {
-    	    		final Node artistNameNode = artistElement.getChildNodes().item(0);
-    	    		//artist = artistNameNode.getNodeName();
-    	    		if(artistNameNode.getNodeName().equals("name")) {
-    	    	    	artist = artistNameNode.getChildNodes().item(0).getNodeValue();
-    	    	    }
-    	    		
-    	    	}
-    	    	
-    	    	// Release date
-    	    	final Element dateElement = (Element) recordingElement.getElementsByTagName("date").item(0);
-    	    	if(dateElement != null) {
-    	    		releaseDate = dateElement.getChildNodes().item(0).getNodeValue();
-    	    	}
-    	    	
-    	    	final String separatorToken = ":%:";
-    	    	
-    	    	// SongTitle+Artist is unique, add it
-    	    	if(uniqueSongMap.get(track+separatorToken+artist) == null) {
-    	    		final MusicbrainzResult result = new MusicbrainzResult();
-        	    	result.setMbid(mbid);
-        	    	result.setTrackName(track);
-        	    	result.setArtistName(artist);    	    	
-        	    	result.setReleaseDate(releaseDate);
-        	    	add(result);
-        	    	
-        	    	uniqueSongMap.put(track+separatorToken+artist, "unique");
-    	    	}
-    	    	
-    	    }
-
-    	  } catch (final DOMException e) {
-    		  // TODO: Handle failed XML parsing 
-    	    //Window.alert("Could not parse XML document.");
-    	  }
+	public MusicbrainzResultList(final AutocompleteJSON json) {
+		
+		// Use JS Overlays to turn JSON into MusicbrainzResult objects
+		
+   		final JsArray<JSONResult> results = json.getEntries();
+        for (int i = 0; i < results.length(); i++) {
+        	
+        	final JSONResult jsonResult = results.get(i);
+        	
+        	final MusicbrainzResult result = new MusicbrainzResult();
+        	result.setTrackName(jsonResult.getTrackName());
+        	result.setArtistName(jsonResult.getArtistName());
+        	result.setMbid(jsonResult.getMbid());
+	    	add(result);
+	    }	
 	}
-
 }
