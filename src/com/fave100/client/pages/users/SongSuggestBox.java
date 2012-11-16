6518229646646e412054a8678b4209886e8ec246
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2012-11-16 14:52:06 -0500

    Matched with new autocomplete API

--- src/com/fave100/client/pages/users/SongSuggestBox.java
+++ src/com/fave100/client/pages/users/SongSuggestBox.java
@@ -21,49 +21,49 @@ import com.google.gwt.user.client.ui.SuggestBox;
  * @author yissachar.radcliffe
  *
  */
-public class SongSuggestBox extends SuggestBox{ 
-	
+public class SongSuggestBox extends SuggestBox{
+
 	private MusicSuggestionOracle suggestions;
 	private HashMap<String, MusicbrainzResult> itemSuggestionMap;
 	private Timer suggestionsTimer;
-		
-	public SongSuggestBox(final MusicSuggestionOracle suggestions, final ApplicationRequestFactory requestFactory) {		
+
+	public SongSuggestBox(final MusicSuggestionOracle suggestions, final ApplicationRequestFactory requestFactory) {
 		super(suggestions);
 		this.suggestions = suggestions;
-		itemSuggestionMap = new HashMap<String, MusicbrainzResult>();	
-		
+		itemSuggestionMap = new HashMap<String, MusicbrainzResult>();
+
 		suggestionsTimer = new Timer() {
 			@Override
 			public void run() {
 				getAutocompleteList();
 			}
 		};
-		
+
 		addKeyUpHandler(new KeyUpHandler() {
 			@Override
-			public void onKeyUp(final KeyUpEvent event) {	
+			public void onKeyUp(final KeyUpEvent event) {
 				//To restrict amount of queries, don't bother searching unless more than 200ms have passed
-				//since the last keystroke.		
+				//since the last keystroke.
 				suggestionsTimer.cancel();
 				// Don't search if it was just an arrow key being pressed
 				if(!KeyCodeEvent.isArrow(event.getNativeKeyCode()) && event.getNativeKeyCode() != KeyCodes.KEY_ENTER) {
 					// Min delay 200ms, Max 1500
-					int delay = 200+(20*getText().length());					
+					int delay = 200+(20*getText().length());
 					if(delay > 1500) delay = 1500;
 					suggestionsTimer.schedule(delay);
 				}
 			}
 		});
-	}	
+	}
 
 	private void getAutocompleteList() {
 		// TODO: Needs to be hosted on actual server
 		String url = SearchPresenter.BASE_SEARCH_URL;
-		url += "search?limit=5&song="+this.getValue();
-		
+		url += "autocomplete?song="+this.getValue();
+
 		 final JsonpRequestBuilder jsonp = new JsonpRequestBuilder();
 		 jsonp.requestObject(url, new AsyncCallback<AutocompleteJSON>() {
-			 
+
 		    @Override
 			public void onFailure(final Throwable throwable) {
 		    	//Window.alert("Fail!");
@@ -71,22 +71,22 @@ public class SongSuggestBox extends SuggestBox{
 
 		    @Override
 			public void onSuccess(final AutocompleteJSON json) {
-			
+
 				// Clear the current suggestions)
 	       		suggestions.clearSuggestions();
 	       		itemSuggestionMap.clear();
-	       		
-	       		// Get the new suggestions from the autocomplete API       	
+
+	       		// Get the new suggestions from the autocomplete API
 	       		final MusicbrainzResultList results = new MusicbrainzResultList(json);
 		        for (int i = 0; i < results.size(); i++) {
 		        	final MusicbrainzResult entry = results.get(i);
-		        	
+
 	       			final String imageUrl = "";
 	       			/*if(entry.getCoverArtUrl() != null) {
 	       				imageUrl = UriUtils.sanitizeUri(entry.getCoverArtUrl());;
 	       			}*/
 	    	    	final String suggestionEntry = "<img src='"+imageUrl+"'/>"+
-	    	    			entry.getTrackName()+"</br><span class='artistName'>"+entry.getArtistName()+"</span>";		    	    	
+	    	    			entry.getTrackName()+"</br><span class='artistName'>"+entry.getArtistName()+"</span>";
 	    	    	String mapEntry = entry.getTrackName();
 	    	    	// Use white space to sneak in duplicate song titles into the hashmap
 	    	    	while(itemSuggestionMap.get(mapEntry) != null) {
@@ -96,16 +96,16 @@ public class SongSuggestBox extends SuggestBox{
 	    	    	// TODO: Safe HTML?
 	    	    	suggestions.addSuggestion(mapEntry, suggestionEntry);
 	    	    }
-	    	    showSuggestionList();		         
+	    	    showSuggestionList();
 		    }
 		});
 		// TODO: type "gr" then backspace twice and type "o", if "gr" request really long
-		// e.g. 6 seconds and "o" request very short, e.g. 100 ms, autocomplete will show results for 
+		// e.g. 6 seconds and "o" request very short, e.g. 100 ms, autocomplete will show results for
 		// "gr" when it should really show for "o"
 		// solution: store requests in array, whenever request completes, clear it and any
-		// other requests with lower index		
+		// other requests with lower index
 	}
-	
+
 	// Returns MusicbrainzResults mapped from the display string passed in
 	public MusicbrainzResult getFromSuggestionMap(final String key) {
 		return itemSuggestionMap.get(key);
