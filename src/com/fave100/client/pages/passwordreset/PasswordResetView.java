15984ca596c9a6aca2abf1f366d7c5f53e2d587c
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2012-11-19 15:25:36 -0500

    Small style changes
    Former-commit-id: b3e8ebee3fe1913f1c9af0a9fa43976afda103e8

--- src/com/fave100/client/pages/passwordreset/PasswordResetView.java
+++ src/com/fave100/client/pages/passwordreset/PasswordResetView.java
@@ -1,10 +1,12 @@
 package com.fave100.client.pages.passwordreset;
 
+import com.fave100.client.pages.BasePresenter;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.FormPanel;
 import com.google.gwt.user.client.ui.FormPanel.SubmitEvent;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
@@ -14,10 +16,11 @@ public class PasswordResetView extends ViewWithUiHandlers<PasswordResetUiHandler
 		PasswordResetPresenter.MyView {
 
 	private final Widget widget;
+	@UiField HTMLPanel topBar;
 	@UiField FormPanel passwordResetForm;
 	@UiField TextBox usernameInput;
 	@UiField TextBox emailInput;
-	
+
 	public interface Binder extends UiBinder<Widget, PasswordResetView> {
 	}
 
@@ -30,7 +33,17 @@ public class PasswordResetView extends ViewWithUiHandlers<PasswordResetUiHandler
 	public Widget asWidget() {
 		return widget;
 	}
-	
+
+	@Override
+	public void setInSlot(final Object slot, final Widget content) {
+		if(slot == BasePresenter.TOP_BAR_SLOT) {
+			topBar.clear();
+			if(content != null) {
+				topBar.add(content);
+			}
+		}
+	}
+
 	@UiHandler("passwordResetForm")
 	public void onSubmit(final SubmitEvent event) {
 		getUiHandlers().sendEmail(usernameInput.getValue(), emailInput.getValue());
