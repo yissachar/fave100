6e04eed93be6c3b9da38ea3b335156cab96bfd96
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2012-11-19 14:40:29 -0500

    Removed date from faveList display

--- src/com/fave100/client/widgets/FaveListCellBase.java
+++ src/com/fave100/client/widgets/FaveListCellBase.java
@@ -9,7 +9,7 @@ import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.client.Window;
 
 public abstract class FaveListCellBase extends AbstractCell<SongProxy>{
-	
+
 	protected ApplicationRequestFactory requestFactory;
 
 	public FaveListCellBase(final ApplicationRequestFactory requestFactory) {
@@ -21,38 +21,38 @@ public abstract class FaveListCellBase extends AbstractCell<SongProxy>{
 	public void render(final Context context, final SongProxy object,
 			final SafeHtmlBuilder sb) {
 		if(object == null) return;
-		
+
 		sb.appendHtmlConstant(getRank(context));
 		sb.appendHtmlConstant(getTrackName(object));
 		sb.appendHtmlConstant(getArtistName(object));
-		sb.appendHtmlConstant(getReleaseDate(object));
+		//sb.appendHtmlConstant(getReleaseDate(object));
 		sb.appendHtmlConstant(getImageThumb(object));
 	}
 
-	
-	
+
+
 	protected String getRank(final Context context) {
 		final String rank = "<span class='faveListRank'>"+(context.getIndex()+1)+".</span>";
 		return rank;
 	}
-	
+
 	protected String getTrackName(final SongProxy object) {
 		String songUrl = Window.Location.getPath();
-		songUrl += Window.Location.getQueryString()+"#"+NameTokens.song+";song=";		
+		songUrl += Window.Location.getQueryString()+"#"+NameTokens.song+";song=";
 		songUrl += URL.encodeQueryString(object.getTrackName());
 		songUrl += ";artist="+URL.encodeQueryString(object.getArtistName());
 		songUrl = URL.encode(songUrl);
-		
+
 		String trackName = "<a href='"+songUrl.replace("'", "%27")+"'";
 		trackName += "class='faveListTrackName'>"+object.getTrackName()+"</a>";
 		return(trackName);
 	}
-	
+
 	protected String getArtistName(final SongProxy object) {
 		final String artistName = "<span class='faveListArtistName'>"+object.getArtistName()+"</span>";
 		return(artistName);
 	}
-	
+
 	protected String getReleaseDate(final SongProxy object) {
 		String releaseDate = "<span class='faveListReleaseDate'>";
 		final String dateVal = object.getReleaseDate();
@@ -62,7 +62,7 @@ public abstract class FaveListCellBase extends AbstractCell<SongProxy>{
 	    releaseDate += "</span>";
 	    return releaseDate;
 	}
-	
+
 	protected String getImageThumb(final SongProxy object) {
 		String imageThumb = "<img class='faveListImageThumb'";
 		imageThumb += "src='"+object.getCoverArtUrl()+"'/>";
--- src/com/fave100/client/widgets/NonpersonalFaveListCell.java
+++ src/com/fave100/client/widgets/NonpersonalFaveListCell.java
@@ -14,37 +14,37 @@ import com.google.web.bindery.requestfactory.shared.Request;
 import com.google.web.bindery.requestfactory.shared.ServerFailure;
 
 public class NonpersonalFaveListCell extends FaveListCellBase{
-	
+
 	public NonpersonalFaveListCell(final ApplicationRequestFactory requestFactory) {
 		super(requestFactory);
 	}
-	
+
 	@Override
 	public void render(final Context context, final SongProxy object,
 			final SafeHtmlBuilder sb) {
 		if(object == null) return;
-		
+
 		sb.appendHtmlConstant(getRank(context));
 		sb.appendHtmlConstant(getTrackName(object));
 		sb.appendHtmlConstant(getArtistName(object));
-		sb.appendHtmlConstant(getReleaseDate(object));
-		
+		//sb.appendHtmlConstant(getReleaseDate(object));
+
 		final String addButton = "<button class='faveListAddButton'>+</button>";
 		sb.appendHtmlConstant(addButton);
-		
+
 		sb.appendHtmlConstant(getImageThumb(object));
-		
+
 		final String whyline = "<span class='faveListWhyline'>"+object.getWhyline()+"</span>";
 		sb.appendHtmlConstant(whyline);
 	}
-	
+
 	@Override
 	public void onBrowserEvent(final Context context, final Element parent, final SongProxy song,
-		final NativeEvent event, final ValueUpdater<SongProxy> valueUpdater) {	
-		
-		if(song == null) return;		
+		final NativeEvent event, final ValueUpdater<SongProxy> valueUpdater) {
+
+		if(song == null) return;
 		super.onBrowserEvent(context, parent, song, event, valueUpdater);
-		
+
 		if(event.getType().equals("click")) {
 			final Element eventTarget = event.getEventTarget().cast();
 			if(eventTarget.getClassName().contains("faveListAddButton")) {
@@ -52,19 +52,19 @@ public class NonpersonalFaveListCell extends FaveListCellBase{
 				final FaveListRequest faveListRequest = requestFactory.faveListRequest();
 				final Request<Void> addFaveReq = faveListRequest.addFaveItemForCurrentUser(FaveList.DEFAULT_HASHTAG,
 						song.getId(), song.getTrackName(), song.getArtistName());
-				
+
 				addFaveReq.fire(new Receiver<Void>() {
 					@Override
 					public void onSuccess(final Void added) {
-						SideNotification.show("Added!");												
+						SideNotification.show("Added!");
 					}
-					
+
 					@Override
 					public void onFailure(final ServerFailure failure) {
 						SideNotification.show(failure.getMessage().replace("Server Error:", ""), true);
 					}
 				});
-				
+
 			}
 		}
 	}
