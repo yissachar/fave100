fb76089dff92b0a4d4fa1dd12f305c8e5e1d5db2
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2012-11-26 14:58:54 -0500

    Changed FaveFeed to use widgets
    Former-commit-id: 5cc99b78189a8260bf6eeee2d41c88d8f4b6d7c9

--- src/com/fave100/client/pagefragments/favefeed/FaveFeedPresenter.java
+++ src/com/fave100/client/pagefragments/favefeed/FaveFeedPresenter.java
@@ -2,12 +2,8 @@ package com.fave100.client.pagefragments.favefeed;
 
 import java.util.List;
 
-import com.fave100.client.place.NameTokens;
 import com.fave100.client.requestfactory.ApplicationRequestFactory;
 import com.google.gwt.event.shared.EventBus;
-import com.google.gwt.safehtml.shared.SafeHtml;
-import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
-import com.google.gwt.user.client.ui.InlineHyperlink;
 import com.google.inject.Inject;
 import com.google.web.bindery.requestfactory.shared.Receiver;
 import com.google.web.bindery.requestfactory.shared.Request;
@@ -19,75 +15,39 @@ public class FaveFeedPresenter extends
 		PresenterWidget<FaveFeedPresenter.MyView> {
 
 	public interface MyView extends View {
-		void setFaveFeedContent(SafeHtml html);
+		void setFaveFeedContent(List<String> activityList);
 	}
 
 	private ApplicationRequestFactory requestFactory;
-	
+
 	@Inject
-	public FaveFeedPresenter(final EventBus eventBus, final MyView view, 
+	public FaveFeedPresenter(final EventBus eventBus, final MyView view,
 			final ApplicationRequestFactory requestFactory) {
 		super(eventBus, view);
 		this.requestFactory = requestFactory;
 	}
-	
+
 	@Override
 	protected void onBind() {
 		super.onBind();
-		
+
 	}
-	
+
 	@Override
 	protected void onReveal() {
 		super.onReveal();
-		
+
 		// Update the FaveFeed
 		final Request<List<String>> faveFeedReq = requestFactory.appUserRequest().getFaveFeedForCurrentUser();
 		faveFeedReq.fire(new Receiver<List<String>>() {
 			@Override
 			public void onSuccess(final List<String> faveFeed) {
-				// TODO: Should be a widget
-				final SafeHtmlBuilder builder = new SafeHtmlBuilder();
-				builder.appendHtmlConstant("<h4 class>");
-				builder.appendEscaped("FaveFeed");
-				builder.appendHtmlConstant("</h4>");
-				if(faveFeed.size() > 0) {
-					builder.appendHtmlConstant("<ul>");
-					for(final String notification : faveFeed) {
-						builder.appendHtmlConstant("<li>");					
-						builder.appendHtmlConstant(notification);
-						builder.appendHtmlConstant("</li>");
-					}
-					builder.appendHtmlConstant("</ul>");
-				} else {
-					builder.appendHtmlConstant("<p>");
-					builder.appendEscaped("No recent activity.");
-					builder.appendHtmlConstant("</p>");
-				}
-				//getView().setVisible(true);
-				//getView().setHTML(builder.toSafeHtml());
-				getView().setFaveFeedContent(builder.toSafeHtml());
+				getView().setFaveFeedContent(faveFeed);
 			}
-			
-			//TODO: Turn fave feed into part of base presenter
+
 			@Override
 			public void onFailure(final ServerFailure failure) {
-				// TODO: What if the error isn't login error?
-				//getView().getFaveFeed().setVisible(false);
-				final SafeHtmlBuilder builder = new SafeHtmlBuilder();
-				builder.appendHtmlConstant("<h4>");
-				builder.appendEscaped("Get your list on!");
-				builder.appendHtmlConstant("</h4>");
-				builder.appendHtmlConstant("<p>");
-				String txt = "If you could only pick the cream of the crop, what ";
-				txt += "song would you choose?";
-				builder.appendEscaped(txt);
-				builder.appendHtmlConstant("</p>");
-				final InlineHyperlink link = new InlineHyperlink();
-				link.setTargetHistoryToken(NameTokens.login);
-				link.setText("Try out Fave100 today");
-				builder.appendHtmlConstant(link.toString());
-				getView().setFaveFeedContent(builder.toSafeHtml());
+				getView().setFaveFeedContent(null);
 			}
 		});
 	}
--- src/com/fave100/client/pagefragments/favefeed/FaveFeedView.java
+++ src/com/fave100/client/pagefragments/favefeed/FaveFeedView.java
@@ -1,9 +1,13 @@
 package com.fave100.client.pagefragments.favefeed;
 
-import com.google.gwt.safehtml.shared.SafeHtml;
+import java.util.List;
+
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.InlineHTML;
+import com.google.gwt.user.client.ui.InlineHyperlink;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewImpl;
@@ -14,8 +18,12 @@ public class FaveFeedView extends ViewImpl implements FaveFeedPresenter.MyView {
 
 	public interface Binder extends UiBinder<Widget, FaveFeedView> {
 	}
-	
+
 	@UiField InlineHTML panel;
+	@UiField Label faveFeedTitle;
+	@UiField InlineHTML activityPanel;
+	@UiField Label faveFeedMessage;
+	@UiField InlineHyperlink loginLink;
 
 	@Inject
 	public FaveFeedView(final Binder binder) {
@@ -28,7 +36,32 @@ public class FaveFeedView extends ViewImpl implements FaveFeedPresenter.MyView {
 	}
 
 	@Override
-	public void setFaveFeedContent(final SafeHtml html) {
-		panel.setHTML(html);
+	public void setFaveFeedContent(final List<String> activityList) {
+		if (activityList != null) {
+			activityPanel.setVisible(true);
+			loginLink.setVisible(false);
+			faveFeedTitle.setText("FaveFeed");
+			if(activityList.size() > 0) {
+				// Show the activity
+				faveFeedMessage.setVisible(false);
+				final SafeHtmlBuilder builder = new SafeHtmlBuilder();
+				for(final String activity : activityList) {
+					builder.appendHtmlConstant(activity);
+					activityPanel.setHTML(builder.toSafeHtml());
+				}
+			} else {
+				faveFeedMessage.setVisible(true);
+				faveFeedMessage.setText("No recent activity");
+			}
+		} else {
+			// Show default message
+			faveFeedTitle.setText("Get your list on!");
+			activityPanel.setVisible(false);
+			String txt = "If you could only pick the cream of the crop, what ";
+			txt += "song would you choose?";
+			faveFeedMessage.setVisible(true);
+			faveFeedMessage.setText(txt);
+			loginLink.setVisible(true);
+		}
 	}
 }
