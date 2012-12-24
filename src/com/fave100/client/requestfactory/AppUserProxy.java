497a80e1ba1ed7523127f877815d553b33d900dc
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2012-12-24 16:40:45 -0500

    Added comment about security risk of sending user email
    Former-commit-id: c2f02ea933d43322fd07bdc27f3a88beaf41bdae

--- src/com/fave100/client/requestfactory/AppUserProxy.java
+++ src/com/fave100/client/requestfactory/AppUserProxy.java
@@ -5,10 +5,13 @@ import com.google.web.bindery.requestfactory.shared.EntityProxy;
 import com.google.web.bindery.requestfactory.shared.ProxyFor;
 
 @ProxyFor(AppUser.class)
-public interface AppUserProxy extends EntityProxy{	
+public interface AppUserProxy extends EntityProxy{
 	Integer getVersion();
-	
+
 	String getUsername();
+	// TODO: Having email here is a security risk
+	// Instead should be a RequestFactory method
+	// Since we will only call it for current user
 	String getEmail();
 	String getAvatarImage();
 }
