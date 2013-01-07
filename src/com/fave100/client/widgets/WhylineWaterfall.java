33ec81ebac56435269832db52feed301b5f42616
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-01-07 12:37:30 -0500

    Added sample WhylineWidget
    Former-commit-id: 9195db732bea6e490215a361a53066f318897352

--- src/com/fave100/client/widgets/WhylineWaterfall.java
+++ src/com/fave100/client/widgets/WhylineWaterfall.java
@@ -2,14 +2,13 @@ package com.fave100.client.widgets;
 
 import com.fave100.client.requestfactory.ApplicationRequestFactory;
 import com.google.gwt.user.client.ui.Grid;
-import com.google.gwt.user.client.ui.Label;
 
 public class WhylineWaterfall extends Grid{
-		
+
 	public WhylineWaterfall(final ApplicationRequestFactory requestFactory) {
 		super(1, 1);
-		
-		this.setWidget(0, 0, new Label("test"));
+
+		this.setWidget(0, 0, new WhylineWidget());
 	}
 
 }
