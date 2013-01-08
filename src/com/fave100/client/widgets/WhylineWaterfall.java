54fafa86e761729b8a9435df571f3903549f879f
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-01-08 13:55:33 -0500

    Turned into UiBinder and added header

--- src/com/fave100/client/widgets/WhylineWaterfall.java
+++ src/com/fave100/client/widgets/WhylineWaterfall.java
@@ -5,18 +5,30 @@ import java.util.List;
 import com.fave100.client.requestfactory.ApplicationRequestFactory;
 import com.fave100.client.requestfactory.SongProxy;
 import com.fave100.client.requestfactory.WhylineProxy;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Grid;
-import com.google.inject.Inject;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
 import com.google.web.bindery.requestfactory.shared.Receiver;
 import com.google.web.bindery.requestfactory.shared.Request;
 
-public class WhylineWaterfall extends Grid {
+public class WhylineWaterfall extends Composite {
 
+	private static WhylineWaterUiBinder uiBinder = GWT
+			.create(WhylineWaterUiBinder.class);
+
+	interface WhylineWaterUiBinder extends UiBinder<Widget, WhylineWaterfall> {
+	}
+
+	@UiField Label header;
+	@UiField Grid grid;
 	private ApplicationRequestFactory requestFactory;
 
-	@Inject
 	public WhylineWaterfall(final ApplicationRequestFactory requestFactory) {
-		super(0, 0);
+		initWidget(uiBinder.createAndBindUi(this));
 		this.requestFactory = requestFactory;
 	}
 
@@ -26,17 +38,19 @@ public class WhylineWaterfall extends Grid {
 		whylineReq.fire(new Receiver<List<WhylineProxy>>() {
 			@Override
 			public void onSuccess(final List<WhylineProxy> whylineList) {
-				clear();
+				grid.clear();
+				header.setVisible(false);
+				if(whylineList.size() > 0) header.setVisible(true);
 				final int rows = (whylineList.size() / 3) + 1;
 				final int columns = (whylineList.size() < 3) ? whylineList.size() : 3;
-				resize(rows, columns);
+				grid.resize(rows, columns);
 				for(int i = 0; i < rows; i++)
 				{
 					for(int j = 0; j < columns; j++)
 					{
 						final int pos = (i == 0 || j == 0) ? i + j : i * j;
 						final WhylineProxy whyline = whylineList.get(pos);
-						setWidget(i, j, new WhylineWidget(whyline));
+						grid.setWidget(i, j, new WhylineWidget(whyline));
 					}
 				}
 			}
