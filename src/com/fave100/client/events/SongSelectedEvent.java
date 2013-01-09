f9b7081e484ccd4da5856155973ddadada8d2344
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-01-09 11:09:37 -0500

    Moved song search bar to top bar

---
+++ src/com/fave100/client/events/SongSelectedEvent.java
@@ -0,0 +1,41 @@
+package com.fave100.client.events;
+
+import com.fave100.client.requestfactory.SongProxy;
+import com.google.web.bindery.event.shared.Event;
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.HandlerRegistration;
+
+public class SongSelectedEvent extends Event<SongSelectedEvent.Handler> {
+
+    public interface Handler {
+        void onSongSelected(SongSelectedEvent event);
+    }
+
+    private static final Type<SongSelectedEvent.Handler> TYPE =
+        new Type<SongSelectedEvent.Handler>();
+
+    public static HandlerRegistration register(final EventBus eventBus,
+        final SongSelectedEvent.Handler handler) {
+      return eventBus.addHandler(TYPE, handler);
+    }
+
+    private final SongProxy song;
+
+    public SongSelectedEvent(final SongProxy song) {
+        this.song = song;
+    }
+
+    @Override
+    public Type<SongSelectedEvent.Handler> getAssociatedType() {
+        return TYPE;
+    }
+
+    public SongProxy getSong() {
+        return song;
+    }
+
+    @Override
+    protected void dispatch(final Handler handler) {
+        handler.onSongSelected(this);
+    }
+}
--- src/com/fave100/client/pagefragments/topbar/TopBarView.java
+++ src/com/fave100/client/pagefragments/topbar/TopBarView.java
@@ -1,24 +1,33 @@
 package com.fave100.client.pagefragments.topbar;
 
+import com.fave100.client.pages.users.MusicSuggestionOracle;
+import com.fave100.client.pages.users.SongSuggestBox;
 import com.fave100.client.place.NameTokens;
+import com.fave100.client.requestfactory.ApplicationRequestFactory;
 import com.google.gwt.dom.client.SpanElement;
+import com.google.gwt.event.logical.shared.SelectionEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.InlineHyperlink;
+import com.google.gwt.user.client.ui.SuggestBox;
+import com.google.gwt.user.client.ui.SuggestOracle.Suggestion;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
-import com.gwtplatform.mvp.client.ViewImpl;
+import com.gwtplatform.mvp.client.ViewWithUiHandlers;
 
-public class TopBarView extends ViewImpl implements TopBarPresenter.MyView {
+public class TopBarView extends ViewWithUiHandlers<TopBarUiHandlers>
+	implements TopBarPresenter.MyView {
 
 	private final Widget widget;
 
 	public interface Binder extends UiBinder<Widget, TopBarView> {
 	}
 
+	@UiField(provided = true) SuggestBox songSuggestBox;
 	@UiField InlineHyperlink logInLogOutLink;
 	@UiField SpanElement greeting;
 	@UiField Anchor myFave100Link;
@@ -26,7 +35,10 @@ public class TopBarView extends ViewImpl implements TopBarPresenter.MyView {
 	@UiField HTMLPanel loginBox;
 
 	@Inject
-	public TopBarView(final Binder binder) {
+	public TopBarView(final Binder binder, final ApplicationRequestFactory requestFactory) {
+		final MusicSuggestionOracle suggestions = new MusicSuggestionOracle();
+		songSuggestBox = new SongSuggestBox(suggestions, requestFactory);
+		songSuggestBox.getElement().setAttribute("placeholder", "Search songs...");
 		widget = binder.createAndBindUi(this);
 	}
 
@@ -45,6 +57,13 @@ public class TopBarView extends ViewImpl implements TopBarPresenter.MyView {
 				loginBox.add(content);
 			}
 		}
+	}
+
+	@UiHandler("songSuggestBox")
+	void onItemSelected(final SelectionEvent<Suggestion> event) {
+		// Look up the selected song in the song map and add it to user fave list
+		getUiHandlers().songSelected(((SongSuggestBox) songSuggestBox).getFromSuggestionMap(event.getSelectedItem().getReplacementString()));
+		songSuggestBox.setValue("");
 	}
 
 	@Override
