996d0c91e4283bd9b5595d617683690327aa7e03
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-01-28 10:30:35 -0500

    Added a CurrentUser class so we don't have to request each page refresh
    Former-commit-id: 864a361017569db0535c5f3cd51425a2e1067d93

--- src/com/fave100/client/gin/ClientGinjector.java
+++ src/com/fave100/client/gin/ClientGinjector.java
@@ -1,10 +1,14 @@
 package com.fave100.client.gin;
 
+import com.fave100.client.CurrentUser;
 import com.fave100.client.pages.home.HomePresenter;
 import com.fave100.client.pages.login.LoginPresenter;
 import com.fave100.client.pages.logout.LogoutPresenter;
+import com.fave100.client.pages.passwordreset.PasswordResetPresenter;
+import com.fave100.client.pages.profile.ProfilePresenter;
 import com.fave100.client.pages.register.RegisterPresenter;
 import com.fave100.client.pages.search.SearchPresenter;
+import com.fave100.client.pages.song.SongPresenter;
 import com.fave100.client.pages.userlist.UserlistPresenter;
 import com.fave100.client.pages.users.UsersPresenter;
 import com.google.gwt.event.shared.EventBus;
@@ -13,9 +17,6 @@ import com.google.gwt.inject.client.GinModules;
 import com.google.gwt.inject.client.Ginjector;
 import com.gwtplatform.dispatch.client.gin.DispatchAsyncModule;
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
-import com.fave100.client.pages.profile.ProfilePresenter;
-import com.fave100.client.pages.song.SongPresenter;
-import com.fave100.client.pages.passwordreset.PasswordResetPresenter;
 
 @GinModules({ DispatchAsyncModule.class, ClientModule.class })
 public interface ClientGinjector extends Ginjector {
@@ -23,6 +24,8 @@ public interface ClientGinjector extends Ginjector {
 	EventBus getEventBus();
 
 	PlaceManager getPlaceManager();
+
+	CurrentUser getCurrentUser();
 
 	AsyncProvider<HomePresenter> getHomePresenter();
 
