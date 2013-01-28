327c456c16180d1047362c29ceae8640ab759084
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-01-28 11:39:33 -0500

    Use ManualReveal with CurrentUser to redirect if logged in

--- src/com/fave100/client/pages/home/HomePresenter.java
+++ src/com/fave100/client/pages/home/HomePresenter.java
@@ -2,6 +2,7 @@ package com.fave100.client.pages.home;
 
 import java.util.List;
 
+import com.fave100.client.CurrentUser;
 import com.fave100.client.pages.BasePresenter;
 import com.fave100.client.pages.BaseView;
 import com.fave100.client.place.NameTokens;
@@ -19,6 +20,7 @@ import com.gwtplatform.mvp.client.proxy.ProxyPlace;
 
 /**
  * Default page that the user will see.
+ *
  * @author yissachar.radcliffe
  *
  */
@@ -26,30 +28,26 @@ public class HomePresenter extends
 		BasePresenter<HomePresenter.MyView, HomePresenter.MyProxy> {
 
 	public interface MyView extends BaseView {
-		//void updateMasterFaveList(List<SongProxy> faveList);
 		void addUserThumb(AppUserProxy appUser);
 	}
-
-	//@ContentSlot
-	//public static final Type<RevealContentHandler<?>> FAVE_FEED_SLOT = new Type<RevealContentHandler<?>>();
-
-	//@Inject FaveFeedPresenter faveFeed;
 
 	@ProxyCodeSplit
 	@NameToken(NameTokens.home)
 	public interface MyProxy extends ProxyPlace<HomePresenter> {
 	}
 
-	private ApplicationRequestFactory requestFactory;
-	private PlaceManager placeManager;
+	private ApplicationRequestFactory	requestFactory;
+	private PlaceManager				placeManager;
+	private CurrentUser					currentUser;
 
 	@Inject
 	public HomePresenter(final ApplicationRequestFactory requestFactory,
-			final EventBus eventBus, final MyView view,
-			final MyProxy proxy, final PlaceManager placeManager) {
+			final EventBus eventBus, final MyView view, final MyProxy proxy,
+			final PlaceManager placeManager, final CurrentUser currentUser) {
 		super(eventBus, view, proxy);
 		this.requestFactory = requestFactory;
 		this.placeManager = placeManager;
+		this.currentUser = currentUser;
 	}
 
 	@Override
@@ -57,44 +55,38 @@ public class HomePresenter extends
 		super.onBind();
 
 		// Get a list of 4 random users
-	    final Request<List<AppUserProxy>> randomUsers = requestFactory.appUserRequest().getRandomUsers(4);
-	    randomUsers.fire(new Receiver<List<AppUserProxy>>() {
-	    	@Override
-	    	public void onSuccess(final List<AppUserProxy> userList) {
-	    		for(final AppUserProxy appUser : userList) {
-	    			// Create thumbs to display songs from the user's lists
-	    			getView().addUserThumb(appUser);
-	    		}
-	    	}
+		final Request<List<AppUserProxy>> randomUsers = requestFactory
+				.appUserRequest().getRandomUsers(4);
+		randomUsers.fire(new Receiver<List<AppUserProxy>>() {
+			@Override
+			public void onSuccess(final List<AppUserProxy> userList) {
+				for (final AppUserProxy appUser : userList) {
+					// Create thumbs to display songs from the user's lists
+					getView().addUserThumb(appUser);
+				}
+			}
 		});
 	}
 
 	@Override
+	public boolean useManualReveal() {
+		return true;
+	}
+
+	@Override
+	public void prepareFromRequest(final PlaceRequest request) {
+		super.prepareFromRequest(request);
+
+		if (currentUser.isLoggedIn()) {
+			final PlaceRequest placeRequest = new PlaceRequest(NameTokens.users)
+					.with("u", currentUser.getAppUser().getUsername());
+			placeManager.revealPlace(placeRequest);
+		}
+		getProxy().manualReveal(HomePresenter.this);
+	}
+
+	@Override
 	protected void onReveal() {
-	    super.onReveal();
-	    //setInSlot(FAVE_FEED_SLOT, faveFeed);
-
-	    // TODO: Can see the redirect - either use GateKeepers or manual reveal
-	    // If a logged in user accesses #Home redirect to their own page
-	    final Request<AppUserProxy> getLoggedInUserReq =  requestFactory.appUserRequest().getLoggedInAppUser();
-		getLoggedInUserReq.fire(new Receiver<AppUserProxy>() {
-			@Override
-			public void onSuccess(final AppUserProxy appUser) {
-				if(appUser != null) {
-					final PlaceRequest placeRequest = new PlaceRequest(NameTokens.users)
-													.with("u", appUser.getUsername());
-					placeManager.revealPlace(placeRequest);
-				}
-			}
-		});
-
-	    // Get the master Fave list
-	    /*final Request<List<SongProxy>> masterFaveListReq = requestFactory.faveListRequest().getMasterFaveList();
-	    masterFaveListReq.fire(new Receiver<List<SongProxy>>() {
-	    	@Override
-	    	public void onSuccess(final List<SongProxy> masterFaveList) {
-	    		getView().updateMasterFaveList(masterFaveList);
-	    	}
-	    });*/
+		super.onReveal();
 	}
 }
