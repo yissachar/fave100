cfd24962bd86d0e2659485039f5242e930ac820a
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-02-04 11:09:44 -0500

    Comments and code cleanup

--- src/com/fave100/client/pages/song/SongPresenter.java
+++ src/com/fave100/client/pages/song/SongPresenter.java
@@ -27,12 +27,19 @@ import com.gwtplatform.mvp.client.proxy.PlaceRequest;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
 import com.gwtplatform.mvp.client.proxy.RevealRootContentEvent;
 
+/**
+ * A song page that will display details about the song
+ *
+ * @author yissachar.radcliffe
+ *
+ */
 public class SongPresenter extends
-		BasePresenter<SongPresenter.MyView, SongPresenter.MyProxy>
-		implements SongUiHandlers{
+		BasePresenter<SongPresenter.MyView, SongPresenter.MyProxy> implements
+		SongUiHandlers {
 
 	public interface MyView extends BaseView, HasUiHandlers<SongUiHandlers> {
 		void setSongInfo(SongProxy song);
+
 		void setYouTubeVideos(YouTubeSearchListJSON videos);
 	}
 
@@ -41,13 +48,14 @@ public class SongPresenter extends
 	public interface MyProxy extends ProxyPlace<SongPresenter> {
 	}
 
-	private final ApplicationRequestFactory requestFactory;
-	private final PlaceManager placeManager;
-	private SongProxy songProxy;
+	private final ApplicationRequestFactory	requestFactory;
+	private final PlaceManager				placeManager;
+	private SongProxy						songProxy;
 
 	@Inject
 	public SongPresenter(final EventBus eventBus, final MyView view,
-			final MyProxy proxy, final ApplicationRequestFactory requestFactory,
+			final MyProxy proxy,
+			final ApplicationRequestFactory requestFactory,
 			final PlaceManager placeManager) {
 		super(eventBus, view, proxy);
 		this.requestFactory = requestFactory;
@@ -71,13 +79,15 @@ public class SongPresenter extends
 
 		// Use parameters to determine what to reveal on page
 		final String song = URL.decode(placeRequest.getParameter("song", ""));
-		final String artist = URL.decode(placeRequest.getParameter("artist", ""));
-		if(song.isEmpty() || artist.isEmpty()) {
+		final String artist = URL.decode(placeRequest
+				.getParameter("artist", ""));
+		if (song.isEmpty() || artist.isEmpty()) {
 			// Malformed request, send the user away
 			placeManager.revealDefaultPlace();
 		} else {
 			// Load the song from the datastore
-			final Request<SongProxy> getSongReq = requestFactory.songRequest().findSongByTitleAndArtist(song, artist);
+			final Request<SongProxy> getSongReq = requestFactory.songRequest()
+					.findSongByTitleAndArtist(song, artist);
 			getSongReq.fire(new Receiver<SongProxy>() {
 				@Override
 				public void onSuccess(final SongProxy song) {
@@ -88,11 +98,13 @@ public class SongPresenter extends
 			});
 
 			// Get any YouTube videos
-			final Request<String> getYoutubeReq = requestFactory.songRequest().getYouTubeResults(song, artist);
+			final Request<String> getYoutubeReq = requestFactory.songRequest()
+					.getYouTubeResults(song, artist);
 			getYoutubeReq.fire(new Receiver<String>() {
 				@Override
 				public void onSuccess(final String json) {
-					final YouTubeSearchListJSON youTubeResults = JsonUtils.safeEval(json);
+					final YouTubeSearchListJSON youTubeResults = JsonUtils
+							.safeEval(json);
 					getView().setYouTubeVideos(youTubeResults);
 				}
 			});
@@ -107,26 +119,36 @@ public class SongPresenter extends
 	@Override
 	public void addSong() {
 
-		if(songProxy == null) return;
-
-		final FaveListRequest faveListRequest = requestFactory.faveListRequest();
+		// Make sure we actually have a song to work with
+		if (songProxy == null)
+			return;
 
 		// Add the song as a FaveItem
-		final Request<Void> addReq = faveListRequest.addFaveItemForCurrentUser(FaveList.DEFAULT_HASHTAG,
-				songProxy.getId(), songProxy.getTitle(), songProxy.getArtist());
+		final FaveListRequest faveListRequest = requestFactory
+				.faveListRequest();
+		final Request<Void> addReq = faveListRequest.addFaveItemForCurrentUser(
+				FaveList.DEFAULT_HASHTAG, songProxy.getId(),
+				songProxy.getTitle(), songProxy.getArtist());
 
 		addReq.fire(new Receiver<Void>() {
 			@Override
 			public void onSuccess(final Void response) {
 				Notification.show("Added");
 			}
+
 			@Override
 			public void onFailure(final ServerFailure failure) {
-				if(failure.getExceptionType().equals(NotLoggedInException.class.getName())) {
-					placeManager.revealPlace(new PlaceRequest(NameTokens.login));
-				} else if(failure.getExceptionType().equals(SongLimitReachedException.class.getName())) {
-					Notification.show("You cannot have more than 100 songs in list");
-				} else if (failure.getExceptionType().equals(SongAlreadyInListException.class.getName())) {
+				// Alert the user if adding the song fails for any reason
+				if (failure.getExceptionType().equals(
+						NotLoggedInException.class.getName())) {
+					placeManager
+							.revealPlace(new PlaceRequest(NameTokens.login));
+				} else if (failure.getExceptionType().equals(
+						SongLimitReachedException.class.getName())) {
+					Notification
+							.show("You cannot have more than 100 songs in list");
+				} else if (failure.getExceptionType().equals(
+						SongAlreadyInListException.class.getName())) {
 					Notification.show("The song is already in your list");
 				}
 			}
@@ -135,6 +157,6 @@ public class SongPresenter extends
 	}
 }
 
-interface SongUiHandlers extends UiHandlers{
+interface SongUiHandlers extends UiHandlers {
 	void addSong();
 }
