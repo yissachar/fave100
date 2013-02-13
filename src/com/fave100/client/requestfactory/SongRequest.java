2c8a9c7cc1b400331a83d15b9c34ec32d3ec29ca
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-02-13 12:30:57 -0500

    Removed unused request
    Former-commit-id: 743a4dcaa323aa67c64703e43904161a23a5829a

--- src/com/fave100/client/requestfactory/SongRequest.java
+++ src/com/fave100/client/requestfactory/SongRequest.java
@@ -8,9 +8,8 @@ import com.google.web.bindery.requestfactory.shared.RequestContext;
 import com.google.web.bindery.requestfactory.shared.Service;
 
 @Service(Song.class)
-public interface SongRequest extends RequestContext{
+public interface SongRequest extends RequestContext {
 
-	Request<SongProxy> findSong(String id);
 	Request<SongProxy> findSongByTitleAndArtist(String title, String artist);
 	Request<String> getYouTubeResults(String song, String artist);
 	Request<List<SongProxy>> getAutocomplete(String songTerm);
