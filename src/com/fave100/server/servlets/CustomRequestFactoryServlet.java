4e5cc1050e11c9f6e53f992d2a1b114c53bad09d
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-03-01 12:12:14 -0500

    Added comment explaining purpose of CustomRequestFactoryServlet

--- src/com/fave100/server/servlets/CustomRequestFactoryServlet.java
+++ src/com/fave100/server/servlets/CustomRequestFactoryServlet.java
@@ -4,13 +4,21 @@ import com.google.web.bindery.requestfactory.server.ExceptionHandler;
 import com.google.web.bindery.requestfactory.server.RequestFactoryServlet;
 import com.google.web.bindery.requestfactory.shared.ServerFailure;
 
+/**
+ * This custom RequestFactory servlet allows for the passing of custom server
+ * errors to the client in a RequestFActory request.
+ *
+ * @author yissachar.radcliffe
+ *
+ */
 @SuppressWarnings("serial")
-public class CustomRequestFactoryServlet extends RequestFactoryServlet{
+public class CustomRequestFactoryServlet extends RequestFactoryServlet {
 
 	static class LoquaciousExceptionHandler implements ExceptionHandler {
 		@Override
 		public ServerFailure createServerFailure(final Throwable throwable) {
-			return new ServerFailure(throwable.getMessage(), throwable.getClass().getName(), null, true);
+			return new ServerFailure(throwable.getMessage(), throwable
+					.getClass().getName(), null, true);
 		}
 	}
 
