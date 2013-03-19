41bf6864d8e956c55588775b03e8a6d7ad34ca75
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-03-19 14:07:48 -0400

    Moved login check to app load instead of TopBar load. Fixes #18

--- src/com/fave100/client/Fave100.java
+++ src/com/fave100/client/Fave100.java
@@ -1,8 +1,14 @@
 package com.fave100.client;
 
+import com.fave100.client.events.CurrentUserChangedEvent;
 import com.fave100.client.gin.ClientGinjector;
+import com.fave100.shared.requestfactory.AppUserProxy;
+import com.fave100.shared.requestfactory.ApplicationRequestFactory;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.web.bindery.requestfactory.shared.Receiver;
+import com.google.web.bindery.requestfactory.shared.Request;
+import com.google.web.bindery.requestfactory.shared.ServerFailure;
 import com.gwtplatform.mvp.client.DelayedBindRegistry;
 
 public class Fave100 implements EntryPoint {
@@ -21,6 +27,23 @@ public class Fave100 implements EntryPoint {
 		// This is required for Gwt-Platform proxy's generator
 		DelayedBindRegistry.bind(ginjector);
 
-		ginjector.getPlaceManager().revealCurrentPlace();
+		// On first page load or page refresh, check for an existing logged in user
+		final ApplicationRequestFactory requestFactory = GWT.create(ApplicationRequestFactory.class);
+		requestFactory.initialize(ginjector.getEventBus());
+		final Request<AppUserProxy> request = requestFactory.appUserRequest().getLoggedInAppUser();
+		request.fire(new Receiver<AppUserProxy>() {
+			@Override
+			public void onSuccess(final AppUserProxy appUser) {
+				ginjector.getEventBus().fireEvent(new CurrentUserChangedEvent(appUser));
+				ginjector.getPlaceManager().revealCurrentPlace();
+
+			}
+
+			@Override
+			public void onFailure(final ServerFailure failure) {
+				ginjector.getPlaceManager().revealCurrentPlace();
+			}
+		});
+
 	}
 }
