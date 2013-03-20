54fbabf00a8238accb6b1122724250c005f3caf3
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-03-20 15:19:23 -0400

    Switched from GWT SuggestBox to custom implementation
    Former-commit-id: 0d5e0c01f5ee0b9a18f82fa2f25c3d8073378c65

---
+++ src/com/fave100/client/pagefragments/autocomplete/SongAutocompleteView.java
@@ -0,0 +1,121 @@
+package com.fave100.client.pagefragments.autocomplete;
+
+import java.util.List;
+
+import com.fave100.shared.requestfactory.SongProxy;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.MouseMoveEvent;
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.FocusPanel;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+import com.gwtplatform.mvp.client.ViewWithUiHandlers;
+
+public class SongAutocompleteView extends ViewWithUiHandlers<SongAutocompleteUiHandlers> implements
+		SongAutocompletePresenter.MyView {
+
+	private final Widget	widget;
+
+	public interface Binder extends UiBinder<Widget, SongAutocompleteView> {
+	}
+
+	interface SongAutocompleteStyle extends CssResource {
+		String selected();
+		String artistName();
+	}
+
+	@UiField SongAutocompleteStyle	style;
+	@UiField TextBox 				searchBox;
+	@UiField FocusPanel				eventCatcher;
+	@UiField HTMLPanel				resultsArea;
+	@UiField HTMLPanel 				resultsPanel;
+
+	@Inject
+	public SongAutocompleteView(final Binder binder) {
+		widget = binder.createAndBindUi(this);
+		searchBox.getElement().setAttribute("placeholder",
+				"Search songs...");
+		resultsArea.setVisible(false);
+	}
+
+	@Override
+	public Widget asWidget() {
+		return widget;
+	}
+
+	@UiHandler("searchBox")
+	void onKeyUp(final KeyUpEvent event) {
+		// If arrow key pressed
+		if(KeyUpEvent.isArrow(event.getNativeKeyCode())) {
+			// If down or up arrow, adjust song selection accordingly
+			if(event.isDownArrow()) {
+				getUiHandlers().setSelection(1, true);
+			} else if(event.isUpArrow()) {
+				getUiHandlers().setSelection(-1, true);
+			}
+		}else if (KeyCodes.KEY_ENTER == event.getNativeKeyCode()) {
+			// Enter key pressed, add currently selected song to favelist
+			getUiHandlers().songSelected();
+		} else {
+			// Otherwise search for song
+			getUiHandlers().getAutocompleteResults(searchBox.getText());
+		}
+	}
+
+	@UiHandler("eventCatcher")
+	void onHover(final MouseMoveEvent event) {
+		for(int i = 0; i < resultsPanel.getWidgetCount(); i++) {
+			final Widget widget = resultsPanel.getWidget(i);
+			// If mouse move occurs over song suggestion, set it as selected
+			if(event.getClientX() >= widget.getAbsoluteLeft()
+				&& event.getClientX() <= widget.getAbsoluteLeft() + widget.getOffsetWidth()
+				&& event.getClientY() >= widget.getAbsoluteTop()
+				&& event.getClientY() <= widget.getAbsoluteTop() + widget.getOffsetHeight())
+			{
+				getUiHandlers().setSelection(i, false);
+			}
+		}
+	}
+
+	@UiHandler("eventCatcher")
+	void onClick(final ClickEvent event) {
+		getUiHandlers().songSelected();
+	}
+
+	@Override
+	public void setSuggestions(final List<SongProxy> suggestions) {
+		resultsPanel.clear();
+		if(suggestions == null || suggestions.size() == 0) {
+			resultsArea.setVisible(false);
+			return;
+		}
+
+		for(final SongProxy suggestion : suggestions) {
+			final HTMLPanel songItem = new HTMLPanel("");
+			final Label songName = new Label(suggestion.getSong());
+			final Label artistName = new Label(suggestion.getArtist());
+			artistName.getElement().addClassName(style.artistName());
+			songItem.add(songName);
+			songItem.add(artistName);
+			resultsPanel.add(songItem);
+		}
+		resultsArea.setVisible(true);
+
+	}
+
+	@Override
+	public void setSelection(final int selection) {
+		for(int i = 0; i < resultsPanel.getWidgetCount(); i++) {
+			resultsPanel.getWidget(i).getElement().removeClassName(style.selected());
+		}
+		resultsPanel.getWidget(selection).getElement().addClassName(style.selected());
+	}
+}
