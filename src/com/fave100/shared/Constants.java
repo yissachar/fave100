c0cf7dc929881fc8e4d2448ed6517fa56518f33e
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-03-25 11:37:52 -0400

    Changed to production jelastic url

--- src/com/fave100/shared/Constants.java
+++ src/com/fave100/shared/Constants.java
@@ -4,6 +4,7 @@ package com.fave100.shared;
 public class Constants {
 
 	public static final String DEFAULT_HASHTAG = "fave100";//TODO: Switch back to proper Jelastic URL
-	public static final String SEARCH_URL = "http://localhost:8080/fave100/search?";
+	public static final String SEARCH_URL = "http://env-6652118.jelastic.servint.net/1/search?";
+	public static final String LOOKUP_URL = "http://env-6652118.jelastic.servint.net/1/lookup?";
 
 }
