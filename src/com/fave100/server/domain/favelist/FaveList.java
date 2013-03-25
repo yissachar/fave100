35a0ab8686cdea12127ce4a77b8e9612e9bb4f55
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-03-25 11:37:38 -0400

    Fixed issue with lowercase username in favelist
    Former-commit-id: bcfa1eda06286d8f8e63f85dc4ddff89beda6796

--- src/com/fave100/server/domain/favelist/FaveList.java
+++ src/com/fave100/server/domain/favelist/FaveList.java
@@ -33,8 +33,8 @@ public class FaveList extends DatastoreObject{
 	private FaveList() {}
 
 	public FaveList(final String username, final String hashtag) {
-		this.id = username+FaveList.SEPERATOR_TOKEN+hashtag;
-		this.user = Ref.create(Key.create(AppUser.class, username));
+		this.id = username.toLowerCase()+FaveList.SEPERATOR_TOKEN+hashtag;
+		this.user = Ref.create(Key.create(AppUser.class, username.toLowerCase()));
 		this.hashtag = hashtag;
 	}
 
