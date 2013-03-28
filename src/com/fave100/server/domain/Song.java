2122abf6b73774c5480a85bd38f36fbe2d4e31eb
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-03-28 19:17:08 -0400

    Removed unneeded log messages

--- src/com/fave100/server/domain/Song.java
+++ src/com/fave100/server/domain/Song.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import com.fave100.shared.Constants;
 import com.fave100.shared.SongInterface;
@@ -48,9 +46,9 @@ public class Song extends DatastoreObject implements SongInterface {
 	}
 
 	public static Song findSong(final String id) {
-		try {Logger.getAnonymousLogger().log(Level.SEVERE, "starting");
+		try {
 			final String lookupUrl = Constants.LOOKUP_URL+"id="+id;
-		    final URL url = new URL(lookupUrl);Logger.getAnonymousLogger().log(Level.SEVERE, "url is: "+lookupUrl);
+		    final URL url = new URL(lookupUrl);
 		    final URLConnection conn = url.openConnection();
 		    final BufferedReader in = new BufferedReader(new InputStreamReader(
 	    		conn.getInputStream(), "UTF-8"));
@@ -62,7 +60,7 @@ public class Song extends DatastoreObject implements SongInterface {
 			    content += inputLine;
 			}
 			in.close();
-Logger.getAnonymousLogger().log(Level.SEVERE, "Result is: "+content);
+
 			final JsonParser parser = new JsonParser();
 		    final JsonElement jsonElement = parser.parse(content);
 		    final JsonObject jsonSong = jsonElement.getAsJsonObject();
