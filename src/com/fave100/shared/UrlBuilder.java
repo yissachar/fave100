3adef535f742b601c56604ce5e85dc2d7f580193
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-04-05 12:07:59 -0400

    Expanded UrlBuilder to allow retrieval of place token only

--- src/com/fave100/shared/UrlBuilder.java
+++ src/com/fave100/shared/UrlBuilder.java
@@ -6,7 +6,10 @@ public class UrlBuilder {
 	// Up to the client to set this to true on init
 	public static boolean isDevMode = false;
 
+	// The entire URL including place
 	private String url;
+	// Just the place
+	private String placeToken;
 
 	public UrlBuilder(final String placeToken) {
 		url = "";
@@ -16,23 +19,30 @@ public class UrlBuilder {
 			url += "http://fave-100.appspot.com/";
 		}
 
-		url += "#";
-		url += placeToken;
+		this.placeToken = placeToken;
 	}
 
 	public UrlBuilder with(final String param, final String arg) {
-		url += ";" + param + "=" + arg;
+		placeToken += ";" + param + "=" + arg;
 		return this;
 	}
 
 	/* Getters and Setters */
 
 	public String getUrl() {
-		return url;
+		return url + "#" + placeToken;
 	}
 
 	public void setUrl(final String url) {
 		this.url = url;
+	}
+
+	public String getPlaceToken() {
+		return placeToken;
+	}
+
+	public void setPlaceToken(final String placeToken) {
+		this.placeToken = placeToken;
 	}
 
 }
