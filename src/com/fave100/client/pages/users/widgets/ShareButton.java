4e1b874b0186091e473fb91255586963d93b60b5
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-04-29 18:21:08 -0400

    Switched custom twitter button to standard twitter button

--- src/com/fave100/client/pages/users/widgets/ShareButton.java
+++ src/com/fave100/client/pages/users/widgets/ShareButton.java
@@ -1,16 +1,14 @@
 package com.fave100.client.pages.users.widgets;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.AnchorElement;
 import com.google.gwt.dom.client.DivElement;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Widget;
 
 public class ShareButton extends Composite {
@@ -24,9 +22,8 @@ public class ShareButton extends Composite {
 
 	@UiField FocusPanel shareArea;
 	@UiField HTMLPanel socialDropdown;
-	@UiField Image twitterShare;
+	@UiField AnchorElement twitterShare;
 	@UiField DivElement fbLike;
-	private String twitterMessage;
 
 	public ShareButton() {
 		initWidget(uiBinder.createAndBindUi(this));
@@ -35,18 +32,12 @@ public class ShareButton extends Composite {
 
 	}
 
-	@UiHandler("twitterShare")
-	void onTwitterClick(final ClickEvent event) {
-		final String url = "http://twitter.com/share?text=" + twitterMessage + Window.Location.getHref();
-		Window.open(url, "_blank", "width=600, height=300");
-	}
-
 	public void setOwnList(final boolean ownList) {
 		_ownList = ownList;
 	}
 
 	public void setTwitterMessage(final String message) {
-		twitterMessage = message;
+		twitterShare.setAttribute("data-text", message + Window.Location.getHref());
 	}
 
 }
