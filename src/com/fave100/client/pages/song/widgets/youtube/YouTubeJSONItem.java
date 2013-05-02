acbfe1501ba116ea21a5fe6ca25368ce8b798604
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-05-02 17:17:17 -0400

    Initial side whylines
    Former-commit-id: ed230dfeecdcf685f6df9349ffa9c5988a6c5856

---
+++ src/com/fave100/client/pages/song/widgets/youtube/YouTubeJSONItem.java
@@ -0,0 +1,22 @@
+package com.fave100.client.pages.song.widgets.youtube;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+/**
+ * JavaScript Overlay
+ * 
+ */
+public class YouTubeJSONItem extends JavaScriptObject {
+
+	protected YouTubeJSONItem() {
+	}
+
+	public final native String getVideoId() /*-{
+											return this.id.videoId;
+											}-*/;
+
+	public final native String getThumbnail() /*-{
+												return this.snippet.thumbnails['default'].url;
+												}-*/;
+
+}
