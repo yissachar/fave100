59731e5f07c312cad5aa63ad970d0ec0c68f657f
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-05-02 16:17:36 -0400

    Explore animation pauses on mouseover. Fixes #55
    Former-commit-id: b5d20e344310263deb1c312343722f217b52923d

--- src/com/fave100/client/pages/explore/ExploreView.java
+++ src/com/fave100/client/pages/explore/ExploreView.java
@@ -4,14 +4,20 @@ import static com.google.gwt.query.client.GQuery.$;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 import com.fave100.client.pages.BasePresenter;
 import com.fave100.client.pages.explore.widgets.ExploreItem;
 import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOverEvent;
 import com.google.gwt.query.client.Function;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
@@ -25,9 +31,13 @@ public class ExploreView extends ViewWithUiHandlers<ExploreUiHandlers> implement
 	}
 
 	@UiField HTMLPanel topBar;
+	@UiField FocusPanel focusPanel;
 	@UiField DivElement exploreContainer;
+	private LinkedList<ExploreItem> animationQueue = new LinkedList<ExploreItem>();
 	private List<ExploreItem> visibleItems = new ArrayList<ExploreItem>();
 	private int dropPosition = 0;
+	private Timer timer;
+	private boolean paused;
 
 	@Inject
 	public ExploreView(final Binder binder) {
@@ -51,6 +61,16 @@ public class ExploreView extends ViewWithUiHandlers<ExploreUiHandlers> implement
 		super.setInSlot(slot, content);
 	}
 
+	@UiHandler("focusPanel")
+	void onMouseOver(final MouseOverEvent event) {
+		paused = true;
+	}
+
+	@UiHandler("focusPanel")
+	void onMouseOut(final MouseOutEvent event) {
+		paused = false;
+	}
+
 	@Override
 	public void setExploreList(final List<ExploreItem> results) {
 		while (exploreContainer.hasChildNodes()) {
@@ -69,48 +89,57 @@ public class ExploreView extends ViewWithUiHandlers<ExploreUiHandlers> implement
 		// Then, in reverse order float them down screen
 		dropPosition = 1000 - $(exploreContainer).outerHeight(true);
 		Collections.reverse(results);
-		int delay = 0;
-		index = 0;
 		for (final ExploreItem item : results) {
 			// Hide all items
 			$(item).css("opacity", "0");
 			// Animate down and reveal
-			$(item).delay(delay, new Function() {
-				@Override
-				public void f() {
-					visibleItems.add(item);
-					// Scroll new item to list and then scroll everything down
-					dropPosition += $(item).outerHeight(true);
-					$(item).animate("top: '" + dropPosition + "px', opacity: 1", 1000, new Function() {
-						@Override
-						public void f() {
-							for (int i = 0; i < visibleItems.size(); i++) {
-								final ExploreItem visibleItem = visibleItems.get(i);
-								// Too many items on screen, remove last one
-								if (visibleItems.size() > 5 && i == 0) {
-									final String topCss = "top: '" + String.valueOf(Integer.parseInt($(visibleItem).css("top").replace("px", "")) + $(item).outerHeight(true) * 3) + "px'";
-									$(visibleItem).animate(topCss + ", opacity: 0", 1000, new Function() {
-										@Override
-										public void f() {
-											// Remove the item
-											visibleItems.remove(visibleItem);
-											exploreContainer.removeChild(visibleItem.getElement());
-										}
-									});
-								}
-								// Otherwise due a normal scroll
-								else {
-									final String topCss = "top: '" + String.valueOf(Integer.parseInt($(visibleItem).css("top").replace("px", "")) + $(item).outerHeight(true)) + "px'";
-									$(visibleItem).animate(topCss, 1000);
-								}
-							}
-						}
-					});
-				}
-			});
-			delay += 2000;
-			index++;
+			animationQueue.add(item);
+		}
+		// Start the animations
+		timer = new Timer() {
+			@Override
+			public void run() {
+				if (!paused)
+					playAnimation();
+			}
+		};
+		timer.scheduleRepeating(2000);
+
+	}
+
+	private void playAnimation() {
+		final ExploreItem item = animationQueue.removeFirst();
+		if (animationQueue.size() == 0) {
+			timer.cancel();
 		}
 
+		// Scroll new item to list and then scroll everything down
+		visibleItems.add(item);
+		dropPosition += $(item).outerHeight(true);
+		$(item).animate("top: '" + dropPosition + "px', opacity: 1", 1000, new Function() {
+			@Override
+			public void f() {
+				for (int i = 0; i < visibleItems.size(); i++) {
+					final ExploreItem visibleItem = visibleItems.get(i);
+					// Too many items on screen, remove last one
+					if (visibleItems.size() > 5 && i == 0) {
+						final String topCss = "top: '" + String.valueOf(Integer.parseInt($(visibleItem).css("top").replace("px", "")) + $(item).outerHeight(true) * 3) + "px'";
+						$(visibleItem).animate(topCss + ", opacity: 0", 1000, new Function() {
+							@Override
+							public void f() {
+								// Remove the item
+								visibleItems.remove(visibleItem);
+								exploreContainer.removeChild(visibleItem.getElement());
+							}
+						});
+					}
+					// Otherwise due a normal scroll
+					else {
+						final String topCss = "top: '" + String.valueOf(Integer.parseInt($(visibleItem).css("top").replace("px", "")) + $(item).outerHeight(true)) + "px'";
+						$(visibleItem).animate(topCss, 1000);
+					}
+				}
+			}
+		});
 	}
 }
