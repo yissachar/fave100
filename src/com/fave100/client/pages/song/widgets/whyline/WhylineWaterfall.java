e77caecab898c798871e7650b94344e04c4a88fb
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-05-03 10:55:10 -0400

    Improved side whylines
    Former-commit-id: 597fd19af2b64de549530efa321ef4e4bd0ade89

--- src/com/fave100/client/pages/song/widgets/whyline/WhylineWaterfall.java
+++ src/com/fave100/client/pages/song/widgets/whyline/WhylineWaterfall.java
@@ -6,10 +6,12 @@ import com.fave100.shared.requestfactory.ApplicationRequestFactory;
 import com.fave100.shared.requestfactory.SongProxy;
 import com.fave100.shared.requestfactory.WhylineProxy;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.web.bindery.requestfactory.shared.Receiver;
 import com.google.web.bindery.requestfactory.shared.Request;
@@ -22,8 +24,13 @@ public class WhylineWaterfall extends Composite {
 	interface WhylineWaterUiBinder extends UiBinder<Widget, WhylineWaterfall> {
 	}
 
-	@UiField Grid grid;
+	@UiField WhylineWaterfallStyle style;
+	@UiField VerticalPanel container;
 	private ApplicationRequestFactory requestFactory;
+
+	interface WhylineWaterfallStyle extends CssResource {
+		String noWhyline();
+	}
 
 	public WhylineWaterfall(final ApplicationRequestFactory requestFactory) {
 		initWidget(uiBinder.createAndBindUi(this));
@@ -36,21 +43,16 @@ public class WhylineWaterfall extends Composite {
 		whylineReq.fire(new Receiver<List<WhylineProxy>>() {
 			@Override
 			public void onSuccess(final List<WhylineProxy> whylineList) {
-				grid.clear();
-				final int rows = (whylineList.size() / 3) + 1;
-				final int columns = (whylineList.size() < 3) ? whylineList.size() : 3;
-				grid.resize(rows, columns);
-				for (int i = 0; i < rows; i++)
-				{
-					for (int j = 0; j < columns; j++)
-					{
-						final int pos = (i == 0 || j == 0) ? i + j : i * j;
-						final WhylineProxy whyline = whylineList.get(pos);
-						grid.setWidget(i, j, new WhylineWidget(whyline));
-					}
+				container.clear();
+				for (final WhylineProxy whyline : whylineList) {
+					container.add(new WhylineWidget(whyline));
+				}
+				if (whylineList.size() == 0) {
+					final Label label = new Label("No whylines yet");
+					label.addStyleName(style.noWhyline());
+					container.add(label);
 				}
 			}
 		});
 	}
-
 }
--- src/com/fave100/client/pages/song/widgets/whyline/WhylineWidget.java
+++ src/com/fave100/client/pages/song/widgets/whyline/WhylineWidget.java
@@ -4,27 +4,34 @@ import com.fave100.client.pages.users.UsersPresenter;
 import com.fave100.client.place.NameTokens;
 import com.fave100.shared.UrlBuilder;
 import com.fave100.shared.requestfactory.WhylineProxy;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.InlineHyperlink;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 public class WhylineWidget extends Composite {
 
-	public WhylineWidget(final WhylineProxy whyline) {
-		final VerticalPanel container = new VerticalPanel();
-		initWidget(container);
+	private static WhylineWidgetUiBinder uiBinder = GWT.create(WhylineWidgetUiBinder.class);
 
-		final Label whylineLabel = new Label(whyline.getWhyline());
-		container.add(whylineLabel);
+	interface WhylineWidgetUiBinder extends UiBinder<Widget, WhylineWidget> {
+	}
 
-		final InlineHyperlink whylineAuthor = new InlineHyperlink();
+	@UiField Label whyline;
+	@UiField InlineHyperlink userLink;
+
+	public WhylineWidget(final WhylineProxy whylineProxy) {
+		initWidget(uiBinder.createAndBindUi(this));
+
+		whyline.setText(whylineProxy.getWhyline());
+
 		final String userPlace = new UrlBuilder(NameTokens.users)
-				.with(UsersPresenter.USER_PARAM, whyline.getUsername())
+				.with(UsersPresenter.USER_PARAM, whylineProxy.getUsername())
 				.getPlaceToken();
-		whylineAuthor.setTargetHistoryToken(userPlace);
-		whylineAuthor.setText("-" + whyline.getUsername());
-		container.add(whylineAuthor);
+		userLink.setTargetHistoryToken(userPlace);
+		userLink.setText(whylineProxy.getUsername());
 	}
 
 }
