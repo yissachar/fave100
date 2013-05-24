e9e69cabf76ae524316fed6868d3cfaab50102d2
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-05-24 09:52:55 -0400

    Restrict size of avatar to 300kb
    Former-commit-id: d0d854d6d7d0abeda66e841b6afde33889be1bb2

--- src/com/fave100/shared/Constants.java
+++ src/com/fave100/shared/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	public static final String DEFAULT_HASHTAG = "fave100";
 	public static final String SEARCH_URL = "http://env-6652118.jelastic.servint.net/1/search?";
 	public static final String LOOKUP_URL = "http://env-6652118.jelastic.servint.net/1/lookup?";
-	public static final long MAX_AVATAR_SIZE = 1024 * 700; //700kb
+	public static final long MAX_AVATAR_SIZE = 1024 * 300; //300kb
 	public static final int MAX_WHYLINE_LENGTH = 80;
 
 }
