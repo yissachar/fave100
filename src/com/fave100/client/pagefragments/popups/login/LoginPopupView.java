9d40b5f10d71b1fd2f32a8cab1e4f667c654435c
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-07-15 12:25:37 -0400

    Fixed issue with login lightbox not being centered

--- src/com/fave100/client/pagefragments/popups/login/LoginPopupView.java
+++ src/com/fave100/client/pagefragments/popups/login/LoginPopupView.java
@@ -1,7 +1,6 @@
 package com.fave100.client.pagefragments.popups.login;
 
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.web.bindery.event.shared.EventBus;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -10,6 +9,7 @@ import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
+import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.PopupViewImpl;
 
 public class LoginPopupView extends PopupViewImpl implements LoginPopupPresenter.MyView {
