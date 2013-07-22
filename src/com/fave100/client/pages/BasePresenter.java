fb66a4b034d95bb82f20a8b118eb52093332a613
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-07-22 10:50:05 -0400

    When code fragments fail to load, refresh page

--- src/com/fave100/client/pages/BasePresenter.java
+++ src/com/fave100/client/pages/BasePresenter.java
@@ -1,17 +1,21 @@
 package com.fave100.client.pages;
 
 import com.fave100.client.pagefragments.topbar.TopBarPresenter;
-import com.google.web.bindery.event.shared.EventBus;
 import com.google.gwt.event.shared.GwtEvent.Type;
+import com.google.gwt.user.client.Window;
 import com.google.inject.Inject;
+import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.Presenter;
 import com.gwtplatform.mvp.client.annotations.ContentSlot;
+import com.gwtplatform.mvp.client.proxy.AsyncCallFailEvent;
+import com.gwtplatform.mvp.client.proxy.AsyncCallFailHandler;
 import com.gwtplatform.mvp.client.proxy.Proxy;
 import com.gwtplatform.mvp.client.proxy.RevealContentHandler;
 import com.gwtplatform.mvp.client.proxy.RevealRootContentEvent;
 
 public abstract class BasePresenter<V extends BaseView, Proxy_ extends Proxy<?>>
-		extends Presenter<V, Proxy_> {
+		extends Presenter<V, Proxy_>
+		implements AsyncCallFailHandler {
 
 	@ContentSlot public static final Type<RevealContentHandler<?>> TOP_BAR_SLOT = new Type<RevealContentHandler<?>>();
 
@@ -30,6 +34,13 @@ public abstract class BasePresenter<V extends BaseView, Proxy_ extends Proxy<?>>
 	protected void onReveal() {
 		super.onReveal();
 		setInSlot(TOP_BAR_SLOT, topBar);
+	}
+
+	// When a new version of the app is deployed, old async JS fragments will be lost but the app will still attempt to load them
+	// This handler will automatically reload the page when an async JS fragment is not loaded properly
+	@Override
+	public void onAsyncCallFail(final AsyncCallFailEvent asyncCallFailEvent) {
+		Window.Location.reload();
 	}
 
 }
