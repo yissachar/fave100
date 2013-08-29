5b7b438fce609802388decd6b1a6ecb6012663b0
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-08-29 17:38:19 -0400

    Add favelist to home page
    Former-commit-id: 67fb8bf7ac4b2f488f81572850c7b42b8ad7db7a

--- src/com/fave100/client/pages/home/HomePresenter.java
+++ src/com/fave100/client/pages/home/HomePresenter.java
@@ -1,24 +1,27 @@
 package com.fave100.client.pages.home;
 
-import static com.google.gwt.query.client.GQuery.$;
-
 import com.fave100.client.CurrentUser;
 import com.fave100.client.pagefragments.popups.login.LoginPopupPresenter;
 import com.fave100.client.pagefragments.popups.register.RegisterPopupPresenter;
 import com.fave100.client.pages.BasePresenter;
 import com.fave100.client.pages.BaseView;
 import com.fave100.client.pages.lists.ListPresenter;
+import com.fave100.client.pages.lists.widgets.favelist.FavelistPresenter;
 import com.fave100.client.place.NameTokens;
+import com.fave100.shared.Constants;
 import com.fave100.shared.Utils;
+import com.google.gwt.event.shared.GwtEvent.Type;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.HasUiHandlers;
 import com.gwtplatform.mvp.client.UiHandlers;
+import com.gwtplatform.mvp.client.annotations.ContentSlot;
 import com.gwtplatform.mvp.client.annotations.NameToken;
 import com.gwtplatform.mvp.client.annotations.ProxyCodeSplit;
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
 import com.gwtplatform.mvp.client.proxy.PlaceRequest;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
+import com.gwtplatform.mvp.client.proxy.RevealContentHandler;
 
 /**
  * Default page that the user will see.
@@ -42,6 +45,9 @@ public class HomePresenter extends
 	private CurrentUser currentUser;
 	@Inject RegisterPopupPresenter registerPopup;
 	@Inject LoginPopupPresenter loginPopup;
+	@Inject FavelistPresenter masterList;
+
+	@ContentSlot public static final Type<RevealContentHandler<?>> MASTER_LIST_SLOT = new Type<RevealContentHandler<?>>();
 
 	@Inject
 	public HomePresenter(final EventBus eventBus, final MyView view, final MyProxy proxy, final PlaceManager placeManager, final CurrentUser currentUser) {
@@ -75,13 +81,14 @@ public class HomePresenter extends
 	@Override
 	protected void onReveal() {
 		super.onReveal();
-		$("body").css("overflow", "hidden");
+		setInSlot(MASTER_LIST_SLOT, masterList);
+		masterList.setHashtag(Constants.DEFAULT_HASHTAG);
+		masterList.refreshFavelist(false);
 	}
 
 	@Override
 	public void onHide() {
 		super.onHide();
-		$("body").css("overflow", "auto");
 	}
 
 	@Override
--- src/com/fave100/client/pages/home/HomeView.java
+++ src/com/fave100/client/pages/home/HomeView.java
@@ -1,14 +1,11 @@
 package com.fave100.client.pages.home;
 
-import com.fave100.client.pages.lists.ListPresenter;
-import com.fave100.client.place.NameTokens;
-import com.fave100.shared.UrlBuilder;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
@@ -25,12 +22,11 @@ public class HomeView extends ViewWithUiHandlers<HomeUiHandlers> implements Home
 	@UiField Image logoImg;
 	/*@UiField Button registerButton;*/
 	@UiField Button loginButton;
-	@UiField Anchor dummyListLink;
+	@UiField HTMLPanel masterList;
 
 	@Inject
 	public HomeView(final Binder binder) {
 		widget = binder.createAndBindUi(this);
-		dummyListLink.setHref(new UrlBuilder(NameTokens.lists).with(ListPresenter.USER_PARAM, "Fave100").getUrl());
 	}
 
 	@Override
@@ -41,6 +37,12 @@ public class HomeView extends ViewWithUiHandlers<HomeUiHandlers> implements Home
 	@Override
 	public void setInSlot(final Object slot, final IsWidget content) {
 		super.setInSlot(slot, content);
+		if (slot == HomePresenter.MASTER_LIST_SLOT) {
+			masterList.clear();
+			if (content != null) {
+				masterList.add(content);
+			}
+		}
 	}
 
 	@UiHandler("registerButton")
