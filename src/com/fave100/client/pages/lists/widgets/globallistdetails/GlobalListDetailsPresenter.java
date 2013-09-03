be6beb5b34915d46f8a48dee17ff21143a2eabc8
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-09-03 16:57:15 -0400

    Remove built by feature

--- src/com/fave100/client/pages/lists/widgets/globallistdetails/GlobalListDetailsPresenter.java
+++ src/com/fave100/client/pages/lists/widgets/globallistdetails/GlobalListDetailsPresenter.java
@@ -1,8 +1,5 @@
 package com.fave100.client.pages.lists.widgets.globallistdetails;
 
-import java.util.List;
-
-import com.fave100.shared.requestfactory.HashtagProxy;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.PresenterWidget;
@@ -12,7 +9,7 @@ public class GlobalListDetailsPresenter extends PresenterWidget<GlobalListDetail
 
 	public interface MyView extends View {
 
-		void setInfo(String hashtag, List<String> avatars, List<String> users, int listCount);
+		void setInfo(String hashtag);
 
 		void show();
 
@@ -25,7 +22,7 @@ public class GlobalListDetailsPresenter extends PresenterWidget<GlobalListDetail
 		super(eventBus, view);
 	}
 
-	public void setHashtag(final HashtagProxy hashtag) {
-		getView().setInfo("#" + hashtag.getName(), hashtag.getSampledUsersAvatars(), hashtag.getSampledUsersNames(), hashtag.getListCount());
+	public void setHashtag(final String hashtag) {
+		getView().setInfo("#" + hashtag);
 	}
 }
