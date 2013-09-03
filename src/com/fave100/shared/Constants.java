2f36288fe30f18a09efe8c5b6e7f8a51862c4b9f
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-09-03 17:30:20 -0400

    Change default hashtag

--- src/com/fave100/shared/Constants.java
+++ src/com/fave100/shared/Constants.java
@@ -2,7 +2,7 @@ package com.fave100.shared;
 
 public class Constants {
 
-	public static final String DEFAULT_HASHTAG = "fave100";
+	public static final String DEFAULT_HASHTAG = "alltime";
 	public static final String SEARCH_URL = "http://env-6652118.jelastic.servint.net/1/search?";
 	public static final String LOOKUP_URL = "http://env-6652118.jelastic.servint.net/1/lookup?";
 	public static final long MAX_AVATAR_SIZE = 1024 * 300; //300kb
