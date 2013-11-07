5720c8aa08d3595927572651ba4a4e0e040bd01b
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2013-11-07 13:12:46 -0500

    Merge list autocompletes into common base

--- src/com/fave100/client/pages/lists/widgets/autocomplete/list/ListAutocompleteModule.java
+++ src/com/fave100/client/pages/lists/widgets/autocomplete/list/ListAutocompleteModule.java
@@ -1,14 +1,14 @@
 package com.fave100.client.pages.lists.widgets.autocomplete.list;
 
-import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
 import com.fave100.client.pagefragments.popups.addsong.addsong.AddSongModule;
 import com.fave100.client.pages.song.widgets.whyline.WhylineModule;
+import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
 
 public class ListAutocompleteModule extends AbstractPresenterModule {
 	@Override
 	protected void configure() {
 		install(new WhylineModule());
 		install(new AddSongModule());
-		bindSingletonPresenterWidget(ListAutocompletePresenter.class, ListAutocompletePresenter.MyView.class, ListAutocompleteView.class);
+		bindPresenterWidget(ListAutocompletePresenter.class, ListAutocompletePresenter.MyView.class, ListAutocompleteView.class);
 	}
 }
---
+++ src/com/fave100/client/pages/lists/widgets/listmanager/AddListAutocompletePresenter.java
@@ -0,0 +1,42 @@
+package com.fave100.client.pages.lists.widgets.listmanager;
+
+import java.util.List;
+
+import com.fave100.client.events.favelist.ListAddedEvent;
+import com.fave100.client.pages.lists.widgets.autocomplete.list.ListAutocompletePresenter;
+import com.fave100.shared.requestfactory.ApplicationRequestFactory;
+import com.google.inject.Inject;
+import com.google.web.bindery.event.shared.EventBus;
+
+public class AddListAutocompletePresenter extends ListAutocompletePresenter {
+
+	public static final String NEW_LIST_PROMPT = "Create new list: ";
+
+	@Inject
+	AddListAutocompletePresenter(final EventBus eventBus, final MyView view, final ApplicationRequestFactory requestFactory) {
+		super(eventBus, view, requestFactory);
+	}
+
+	@Override
+	public void listSelected() {
+		if (getSelection() < 0)
+			return;
+
+		String selection = _suggestions.get(getSelection());
+		if (selection.startsWith(NEW_LIST_PROMPT))
+			selection = selection.substring(NEW_LIST_PROMPT.length(), selection.length());
+
+		super.listSelected();
+
+		_eventBus.fireEvent(new ListAddedEvent(selection));
+	}
+
+	@Override
+	public void setSuggestions(List<String> suggestions) {
+		if (!suggestions.contains(_lastSearch)) {
+			suggestions.add(NEW_LIST_PROMPT + _lastSearch);
+		}
+		super.setSuggestions(suggestions);
+	}
+
+}
