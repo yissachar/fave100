2127455c88cbad5c8976b7dbecd7af2e71b69833
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2014-01-02 15:28:22 -0500

    Set copyright date to auto-update to current year

--- src/com/fave100/client/pagefragments/footer/Footer.java
+++ src/com/fave100/client/pagefragments/footer/Footer.java
@@ -1,7 +1,11 @@
 package com.fave100.client.pagefragments.footer;
 
+import java.util.Date;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.SpanElement;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -18,8 +22,11 @@ public class Footer extends Composite {
 	interface FooterUiBinder extends UiBinder<Widget, Footer> {
 	}
 
+	@UiField SpanElement year;
+
 	public Footer() {
 		initWidget(uiBinder.createAndBindUi(this));
+		year.setInnerText(String.valueOf(1900 + new Date().getYear()));
 	}
 
 }
