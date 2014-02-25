22f292088fb16ef79f5255ef31cf3239a3dcff11
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2014-02-25 15:45:15 -0500

    Clean up tests a bit

---
+++ src/test/java/com/fave100/server/api/ApiTestSuite.java
@@ -0,0 +1,11 @@
+package com.fave100.server.api;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({SongApiTest.class, WhyLineApiTest.class})
+public class ApiTestSuite {
+
+}
