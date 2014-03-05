64a1f8d6638bd8b2a599a395450decef6b72f20d
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2014-03-05 11:20:43 -0500

    Use SSL for song search

--- src/main/java/com/fave100/shared/Constants.java
+++ src/main/java/com/fave100/shared/Constants.java
@@ -3,8 +3,8 @@ package com.fave100.shared;
 public class Constants {
 
 	public static final String DEFAULT_HASHTAG = "alltime";
-	public static final String SEARCH_URL = "http://env-6652118.jelastic.servint.net/1/search?";
-	public static final String LOOKUP_URL = "http://env-6652118.jelastic.servint.net/1/lookup?";
+	public static final String SEARCH_URL = "https://env-6652118.jelastic.servint.net/1/search?";
+	public static final String LOOKUP_URL = "https://env-6652118.jelastic.servint.net/1/lookup?";
 	public static final long MAX_AVATAR_SIZE = 1024 * 300; //300kb
 	public static final int MAX_WHYLINE_LENGTH = 80;
 	public static final int MAX_HASHTAG_LENGTH = 20;
