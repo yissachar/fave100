2cd7358c661c677b674a27701773e411398d7b73
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2014-03-18 13:34:07 -0400

    Access static methods in a static way
    Former-commit-id: 994d13834dbc2365b3b60c13ac6e4c83516357af

--- src/main/java/com/fave100/server/servlets/HashtagBuilderServlet.java
+++ src/main/java/com/fave100/server/servlets/HashtagBuilderServlet.java
@@ -23,7 +23,6 @@ import com.fave100.server.domain.favelist.FaveRankerWrapper;
 import com.fave100.server.domain.favelist.Hashtag;
 import com.google.appengine.api.datastore.Cursor;
 import com.google.appengine.api.datastore.QueryResultIterator;
-import com.google.inject.Inject;
 import com.googlecode.objectify.cmd.Query;
 
 /**
@@ -38,8 +37,6 @@ public class HashtagBuilderServlet extends HttpServlet
 {
 	public static String HASHTAG_BUILDER_URL = "/tasks/hashtags";
 	public static String HASHTAG_PARAM = "hashtag";
-
-	@Inject FaveListDao faveListDao;
 
 	@Override
 	public void doPost(final HttpServletRequest req, final HttpServletResponse res)
@@ -131,7 +128,7 @@ public class HashtagBuilderServlet extends HttpServlet
 			int i = 1;
 			for (final FaveItem faveItem : faveList.getList()) {
 				final FaveRankerWrapper faveHolder = new FaveRankerWrapper(faveItem);
-				final double score = faveListDao.calculateItemScore(i);
+				final double score = FaveListDao.calculateItemScore(i);
 				final double newVal = (all.get(faveHolder) != null) ? all.get(faveHolder) + score : score;
 				all.put(faveHolder, newVal);
 				i++;
