fc376419ac7e159f432bef11a6c86f04a5798658
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2014-04-02 17:44:32 -0400

    Tidy up Gin modules
    Former-commit-id: cb8e6933eaf72c49d5ebec014bd0ef0c9265e1ba

--- src/main/java/com/fave100/client/gin/ClientModule.java
+++ src/main/java/com/fave100/client/gin/ClientModule.java
@@ -10,21 +10,10 @@ import com.fave100.client.pagefragments.popups.login.LoginPopupPresenter;
 import com.fave100.client.pagefragments.popups.login.LoginPopupView;
 import com.fave100.client.pagefragments.register.RegisterWidgetPresenter;
 import com.fave100.client.pagefragments.register.RegisterWidgetView;
-import com.fave100.client.pagefragments.topbar.TopBarPresenter;
-import com.fave100.client.pagefragments.topbar.TopBarView;
-import com.fave100.client.pages.lists.ListPresenter;
-import com.fave100.client.pages.lists.ListView;
+import com.fave100.client.pagefragments.topbar.TopBarModule;
+import com.fave100.client.pages.about.AboutModule;
+import com.fave100.client.pages.lists.ListModule;
 import com.fave100.client.pages.lists.widgets.autocomplete.list.ListAutocompleteModule;
-import com.fave100.client.pages.lists.widgets.autocomplete.song.SongAutocompletePresenter;
-import com.fave100.client.pages.lists.widgets.autocomplete.song.SongAutocompleteView;
-import com.fave100.client.pages.lists.widgets.favelist.FavelistPresenter;
-import com.fave100.client.pages.lists.widgets.favelist.FavelistView;
-import com.fave100.client.pages.lists.widgets.globallistdetails.GlobalListDetailsPresenter;
-import com.fave100.client.pages.lists.widgets.globallistdetails.GlobalListDetailsView;
-import com.fave100.client.pages.lists.widgets.listmanager.ListManagerPresenter;
-import com.fave100.client.pages.lists.widgets.listmanager.ListManagerView;
-import com.fave100.client.pages.lists.widgets.usersfollowing.UsersFollowingPresenter;
-import com.fave100.client.pages.lists.widgets.usersfollowing.UsersFollowingView;
 import com.fave100.client.pages.login.LoginPresenter;
 import com.fave100.client.pages.login.LoginView;
 import com.fave100.client.pages.passwordreset.PasswordResetPresenter;
@@ -33,15 +22,12 @@ import com.fave100.client.pages.profile.ProfilePresenter;
 import com.fave100.client.pages.profile.ProfileView;
 import com.fave100.client.pages.register.RegisterPresenter;
 import com.fave100.client.pages.register.RegisterView;
-import com.fave100.client.pages.song.SongPresenter;
-import com.fave100.client.pages.song.SongView;
-import com.fave100.client.pages.song.widgets.playlist.PlaylistPresenter;
-import com.fave100.client.pages.song.widgets.playlist.PlaylistView;
-import com.fave100.client.pages.song.widgets.youtube.YouTubePresenter;
-import com.fave100.client.pages.song.widgets.youtube.YouTubeView;
+import com.fave100.client.pages.song.SongModule;
+import com.fave100.client.pages.tour.TourModule;
 import com.fave100.client.place.ClientPlaceManager;
 import com.fave100.client.place.DefaultPlace;
 import com.fave100.client.place.NameTokens;
+import com.fave100.client.widgets.alert.AlertModule;
 import com.fave100.shared.Constants;
 import com.gwtplatform.dispatch.rest.client.RestApplicationPath;
 import com.gwtplatform.dispatch.rest.client.gin.RestDispatchAsyncModule;
@@ -56,7 +42,15 @@ public class ClientModule extends AbstractPresenterModule {
 
 	@Override
 	protected void configure() {
+		// Page and widget modules
+		install(new SongModule());
+		install(new TopBarModule());
+		install(new ListModule());
+		install(new TourModule());
+		install(new AboutModule());
+		install(new AlertModule());
 		install(new ListAutocompleteModule());
+
 		install(new DefaultModule(ClientPlaceManager.class));
 		install(new RestDispatchAsyncModule.Builder().build());
 
@@ -67,61 +61,20 @@ public class ClientModule extends AbstractPresenterModule {
 		bind(GoogleAnalyticsNavigationTracker.class).asEagerSingleton();
 
 		bind(RequestCache.class).asEagerSingleton();
-
 		bind(CurrentUser.class).asEagerSingleton();
-
 		bind(LoggedInGatekeeper.class).asEagerSingleton();
-
 		bind(NotLoggedInGatekeeper.class).asEagerSingleton();
 
 		bindConstant().annotatedWith(DefaultPlace.class).to(NameTokens.lists);
 		bindConstant().annotatedWith(ErrorPlace.class).to(NameTokens.lists);
 		bindConstant().annotatedWith(UnauthorizedPlace.class).to(NameTokens.lists);
 
-		bindSingletonPresenterWidget(TopBarPresenter.class,
-				TopBarPresenter.MyView.class, TopBarView.class);
-
-		bindPresenter(RegisterPresenter.class, RegisterPresenter.MyView.class,
-				RegisterView.class, RegisterPresenter.MyProxy.class);
-
-		bindPresenter(ListPresenter.class, ListPresenter.MyView.class,
-				ListView.class, ListPresenter.MyProxy.class);
-
-		bindPresenter(LoginPresenter.class, LoginPresenter.MyView.class,
-				LoginView.class, LoginPresenter.MyProxy.class);
-
-		bindPresenter(ProfilePresenter.class, ProfilePresenter.MyView.class,
-				ProfileView.class, ProfilePresenter.MyProxy.class);
-
-		bindPresenterWidget(LoginWidgetPresenter.class,
-				LoginWidgetPresenter.MyView.class, LoginWidgetView.class);
-
-		bindPresenter(SongPresenter.class, SongPresenter.MyView.class,
-				SongView.class, SongPresenter.MyProxy.class);
-
-		bindPresenter(PasswordResetPresenter.class,
-				PasswordResetPresenter.MyView.class, PasswordResetView.class,
-				PasswordResetPresenter.MyProxy.class);
-
-		bindSingletonPresenterWidget(SongAutocompletePresenter.class,
-				SongAutocompletePresenter.MyView.class,
-				SongAutocompleteView.class);
-
-		bindSingletonPresenterWidget(FavelistPresenter.class,
-				FavelistPresenter.MyView.class, FavelistView.class);
-
+		bindPresenter(RegisterPresenter.class, RegisterPresenter.MyView.class, RegisterView.class, RegisterPresenter.MyProxy.class);
+		bindPresenter(LoginPresenter.class, LoginPresenter.MyView.class, LoginView.class, LoginPresenter.MyProxy.class);
+		bindPresenter(ProfilePresenter.class, ProfilePresenter.MyView.class, ProfileView.class, ProfilePresenter.MyProxy.class);
+		bindPresenterWidget(LoginWidgetPresenter.class, LoginWidgetPresenter.MyView.class, LoginWidgetView.class);
+		bindPresenter(PasswordResetPresenter.class, PasswordResetPresenter.MyView.class, PasswordResetView.class, PasswordResetPresenter.MyProxy.class);
 		bindSingletonPresenterWidget(LoginPopupPresenter.class, LoginPopupPresenter.MyView.class, LoginPopupView.class);
-
 		bindSingletonPresenterWidget(RegisterWidgetPresenter.class, RegisterWidgetPresenter.MyView.class, RegisterWidgetView.class);
-
-		bindSingletonPresenterWidget(PlaylistPresenter.class, PlaylistPresenter.MyView.class, PlaylistView.class);
-
-		bindSingletonPresenterWidget(YouTubePresenter.class, YouTubePresenter.MyView.class, YouTubeView.class);
-
-		bindPresenterWidget(UsersFollowingPresenter.class, UsersFollowingPresenter.MyView.class, UsersFollowingView.class);
-
-		bindSingletonPresenterWidget(ListManagerPresenter.class, ListManagerPresenter.MyView.class, ListManagerView.class);
-
-		bindSingletonPresenterWidget(GlobalListDetailsPresenter.class, GlobalListDetailsPresenter.MyView.class, GlobalListDetailsView.class);
 	}
 }
