f40f495eabb25e93886b7c8f98cbc0d64e3a9551
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2014-04-02 11:22:41 -0400

    Change trending list

--- src/main/java/com/fave100/server/api/TrendingApi.java
+++ src/main/java/com/fave100/server/api/TrendingApi.java
@@ -38,7 +38,7 @@ public class TrendingApi {
 		trending.add(new StringResult("running"));
 		trending.add(new StringResult("wedding"));
 		trending.add(new StringResult("dance"));
-		trending.add(new StringResult("tvthemes"));
+		trending.add(new StringResult("tvsongs"));
 		trending.add(new StringResult("classicrock"));
 		trending.add(new StringResult("moviesongs"));
 		trending.add(new StringResult("onehitwonders"));
