a88068dc3df8c0dc50c40402c679a2ad64eb4acc
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2014-05-02 16:27:51 -0400

    Fix bug with async loading after deploy

--- src/main/java/com/fave100/client/pages/PagePresenter.java
+++ src/main/java/com/fave100/client/pages/PagePresenter.java
@@ -2,21 +2,16 @@ package com.fave100.client.pages;
 
 import com.fave100.client.pagefragments.topbar.TopBarPresenter;
 import com.google.gwt.event.shared.GwtEvent.Type;
-import com.google.gwt.user.client.Window;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.Presenter;
 import com.gwtplatform.mvp.client.View;
 import com.gwtplatform.mvp.client.annotations.ContentSlot;
-import com.gwtplatform.mvp.client.proxy.AsyncCallFailEvent;
-import com.gwtplatform.mvp.client.proxy.AsyncCallFailHandler;
 import com.gwtplatform.mvp.client.proxy.Proxy;
 import com.gwtplatform.mvp.client.proxy.RevealContentHandler;
 import com.gwtplatform.mvp.client.proxy.RevealRootContentEvent;
 
-public abstract class PagePresenter<V extends View, Proxy_ extends Proxy<?>>
-		extends Presenter<V, Proxy_>
-		implements AsyncCallFailHandler {
+public abstract class PagePresenter<V extends View, Proxy_ extends Proxy<?>> extends Presenter<V, Proxy_> {
 
 	@ContentSlot public static final Type<RevealContentHandler<?>> TOP_BAR_SLOT = new Type<RevealContentHandler<?>>();
 
@@ -30,7 +25,6 @@ public abstract class PagePresenter<V extends View, Proxy_ extends Proxy<?>>
 	@Override
 	protected void onBind() {
 		super.onBind();
-		this.addRegisteredHandler(new Type<AsyncCallFailHandler>(), this);
 	}
 
 	@Override
@@ -42,13 +36,6 @@ public abstract class PagePresenter<V extends View, Proxy_ extends Proxy<?>>
 	protected void onReveal() {
 		super.onReveal();
 		setInSlot(TOP_BAR_SLOT, topBar);
-	}
-
-	// When a new version of the app is deployed, old async JS fragments will be lost but the app will still attempt to load them
-	// This handler will automatically reload the page when an async JS fragment is not loaded properly
-	@Override
-	public void onAsyncCallFail(final AsyncCallFailEvent asyncCallFailEvent) {
-		Window.Location.reload();
 	}
 
 }
