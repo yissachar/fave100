77ce30376ab7439b331d810bb57b39031c95069e
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2014-08-01 16:12:50 -0400

    Point search service to new URL
    Former-commit-id: 13bb2b70dc64c3660f65452e7c198b85ee777c3b

--- src/main/java/com/fave100/shared/Constants.java
+++ src/main/java/com/fave100/shared/Constants.java
@@ -3,8 +3,9 @@ package com.fave100.shared;
 public class Constants {
 
 	public static final String DEFAULT_HASHTAG = "alltime";
-	public static final String SEARCH_URL = "https://env-6652118.jelastic.servint.net/1/search?";
-	public static final String LOOKUP_URL = "https://env-6652118.jelastic.servint.net/1/lookup?";
+	public static final String SEARCH_SERVICE_URL = "http://default-environment-9n6mmewqkp.elasticbeanstalk.com";
+	public static final String SEARCH_URL = SEARCH_SERVICE_URL + "/search?";
+	public static final String LOOKUP_URL = SEARCH_SERVICE_URL + "/lookup?";
 	public static final long MAX_AVATAR_SIZE = 1024 * 300; //300kb
 	public static final int MAX_WHYLINE_LENGTH = 80;
 	public static final int MAX_HASHTAG_LENGTH = 20;
