a9981d3ee382a0c16527a76b3a1aba3f614a3eaf
Yissachar Radcliffe <yissachar.radcliffe@caseware.com>
2014-11-07 14:54:17 -0500

    Add back to list button on song page

--- src/main/java/com/fave100/client/pages/song/SongView.java
+++ src/main/java/com/fave100/client/pages/song/SongView.java
@@ -1,9 +1,15 @@
 package com.fave100.client.pages.song;
 
 import com.fave100.client.pages.PageView;
+import com.fave100.client.widgets.Icon;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
+import com.gwtplatform.mvp.client.proxy.PlaceManager;
 
 public class SongView extends PageView<SongUiHandlers> implements SongPresenter.MyView {
 
@@ -12,14 +18,29 @@ public class SongView extends PageView<SongUiHandlers> implements SongPresenter.
 	public interface Binder extends UiBinder<Widget, SongView> {
 	}
 
+	@UiField Icon backButton;
+	@UiField Label backText;
+	private PlaceManager _placeManager;
+
 	@Inject
-	public SongView(final Binder binder) {
+	public SongView(final Binder binder, PlaceManager placeManager) {
 		widget = binder.createAndBindUi(this);
+		_placeManager = placeManager;
 	}
 
 	@Override
 	public Widget asWidget() {
 		return widget;
+	}
+
+	@UiHandler("backButton")
+	void onBackButtonClick(ClickEvent event) {
+		_placeManager.navigateBack();
+	}
+
+	@UiHandler("backText")
+	void onBackTextClick(ClickEvent event) {
+		_placeManager.navigateBack();
 	}
 
 }
